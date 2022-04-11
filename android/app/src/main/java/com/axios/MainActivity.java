8021b9e06cb2a205e535f8781f8720c6b1190184
Maxence <maxrychel100@gmail.com>
2022-04-11 02:31:53 +0200

    Add bootsplash function load screen

--- android/app/src/main/java/com/axios/MainActivity.java
+++ android/app/src/main/java/com/axios/MainActivity.java
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+import com.zoontek.rnbootsplash.RNBootSplash; // import in android 
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -35,6 +37,13 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable the Fabric Renderer.
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
+    }
+
+    // instance for initialize the splash screen
+    @Override
+    protected void loadApp(String appKey) {
+      RNBootSplash.init(getPlainActivity()); 
+      super.loadApp(appKey);
     }
   }
 }
