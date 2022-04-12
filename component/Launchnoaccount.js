03ab28bb37672244091580ed31f91a6bf899b345
Maxence <maxrychel100@gmail.com>
2022-04-12 04:34:47 +0200

    Create first componnt launchnoaccount.js

---
+++ component/Launchnoaccount.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+const Launchnoaccount = () => {
+    return (
+        <View>
+            <Text>Ok pour la stack</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Launchnoaccount;
