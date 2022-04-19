00e5a83f5d9e62f370ee6a1c009c1deaed65920d
Maxence <maxrychel100@gmail.com>
2022-04-19 03:25:39 +0200

    Update all component

--- component/Getstarting.js
+++ component/Getstarting.js
@@ -1,27 +1,122 @@
 import React, {useState} from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    Dimensions,
-    Animated,
-    ScrollView
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Animated,
+  ScrollView,
 } from 'react-native';
-
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import LinearGradient from 'react-native-linear-gradient';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faSmile, faRightLong} from '@fortawesome/free-solid-svg-icons';
 
 const Getstarting = () => {
-
-    return (
-      <ScrollView>
-        <View>
-          <Text>Welcome : can you help you ?</Text>
+  return (
+    <LinearGradient
+      start={{x: 0.0, y: 0.25}}
+      end={{x: 0.5, y: 1.0}}
+      locations={[0.05, 0.6]}
+      colors={['#FEB692', '#EA5455']}
+      style={styles.ui_splash_todo_contain}>
+      <View style={styles.ui_splash_contain_widget_acxios}>
+        <Text style={styles.body}>
+          <FontAwesome5 name="robot" size={160} color="white" />
+        </Text>
+      </View>
+      <View style={styles.ui_splash_below_title_message}>
+        <Text style={styles.ui_splash_below_text_message}>
+          Faire de votre agence de voyage (par bus) une réalité.{' '}
+        </Text>
+      </View>
+      <View style={styles.ui_splash_box_create_title_message}>
+        <FontAwesomeIcon icon={faSmile} color="#FEB692" size={59} />
+        <View style={styles.ui_splash_button_goto_starting}>
+          <Text style={styles.ui_splash_button_goto_text_starting}>
+            creer une agence
+          </Text>
+          <FontAwesomeIcon icon={faRightLong} color="#ffff" size={25} />
         </View>
-      </ScrollView>
-    )
-}
+        <Text style={styles.ui_splash_contain_other_button_goto}>
+          creer un compte associé à une agence
+        </Text>
+      </View>
+    </LinearGradient>
+  );
+};
 
 const styles = StyleSheet.create({
-
+  ui_splash_todo_contain: {
+    height: Dimensions.get('window').height,
+  },
+  ui_splash_contain_widget_acxios: {
+    width: 190,
+    height: 250,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  body: {
+    color: 'red',
+    transform: [{rotate: '-45deg'}],
+  },
+  ui_splash_below_title_message: {
+    width: '90%',
+    height: 50,
+    marginTop: 40,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#A3BDED',
+    borderRadius: 5,
+  },
+  ui_splash_below_text_message: {
+    fontSize: 16,
+    fontWeight: '300',
+    fontFamily: 'PatrickHandSC-Regular',
+    color: 'white',
+  },
+  ui_splash_box_create_title_message: {
+    width: '90%',
+    height: 230,
+    marginTop: 40,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 40,
+  },
+  ui_splash_button_goto_starting: {
+    width: '70%',
+    height: 50,
+    textAlignVertical: 'center',
+    marginTop: 30,
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F9D29D',
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  ui_splash_button_goto_text_starting: {
+    fontSize: 17,
+    fontFamily: 'Snippet-Regular',
+    marginRight: 5,
+    top: -1,
+    color: 'white',
+  },
+  ui_splash_contain_other_button_goto: {
+    textDecorationLine: 'underline',
+    marginBottom: 5,
+  },
 });
 
 export default Getstarting;
