cdddb6ea7ecd042bd5f62cec484f7ba6984d8be0
Maxence <maxrychel100@gmail.com>
2022-06-02 23:51:56 +0200

    Creating api utils function of  v0.0.1 acxios

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+const api = (paths, value) => {
+    let url = 'https://591b-129-0-76-108.eu.ngrok.io';
+    
+    if(paths == 'POST'){
+        return fetch(url, {
+            method: paths,
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({title: value})
+            }).then((res) => res.json());
+    }
+    else{
+        return fetch(url).then((res) => res.json());
+    }
+}
+
+export default api;
