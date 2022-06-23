59a5ed520d6002888ce7a1f437fc3de5a619e352
Maxence <maxrychel100@gmail.com>
2022-06-23 12:03:25 +0200

    Updating Header bar for all user to v0.0.1 acxios

--- src/components/HeaderUser.js
+++ src/components/HeaderUser.js
@@ -3,23 +3,37 @@ import {
   View,
   Text,
   StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const HeaderUser = (props) => {
-  const {Gtitle, Depart, Arrive} = props;
+  const {Gtitle, Depart, Arrive, onPress} = props;
   
   return (
     <View style={styles.ui_splash_contain_header_globe}>
-      <View style={styles.ui_splash_contain_destination_items_globe2}>
-          <Text style={styles.ui_splash_contain_destination_items_text1}>{Gtitle}</Text>
+      <View style={styles.ui_splash_contain_destination_items_globe_text2}>
+        <TouchableOpacity 
+              activeOpacity={0.7}
+              onPress={onPress}>
+            <View style={styles.ui_splash_contain_globe_cadre_tags}>
+                <FontAwesome5 name="align-justify" size={30} color='white' />
+            </View>
+        </TouchableOpacity>
       </View>
-      <View style={styles.ui_splash_contain_destination_globe}>
-        <View style={styles.ui_splash_contain_destination_items_globe1}>
-            <Text style={styles.ui_splash_contain_destination_items_text2}>agence</Text>
-            <Text style={styles.ui_splash_contain_destination_items_text2}>{Depart}-{Arrive}</Text>
+      <View style={styles.ui_splash_contain_destination_items_contains_globe2}>
+        <View style={styles.ui_splash_contain_destination_items_globe2}>
+            <Text style={styles.ui_splash_contain_destination_items_text1}>{Gtitle}</Text>
         </View>
+        <View style={styles.ui_splash_contain_destination_globe}>
+          <View style={styles.ui_splash_contain_destination_items_globe1}>
+              <Text style={styles.ui_splash_contain_destination_items_text2}>agence</Text>
+              <Text style={styles.ui_splash_contain_destination_items_text2}>{Depart}-{Arrive}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.ui_splash_contain_destination_items_globe_text2}>
         <Text style={styles.ui_splash_contain_destination_items_text2}><FontAwesome5 name="search" size={24} /></Text>
       </View>
     </View>
@@ -29,7 +43,7 @@ const HeaderUser = (props) => {
 const styles = StyleSheet.create({
   ui_splash_contain_header_globe: {
       backgroundColor: '#f44336',
-      flexDirection: 'column',
+      flexDirection: 'row',
       height: 70,
       justifyContent: 'center',
   },
@@ -38,19 +52,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#f4433642',
     justifyContent: 'space-between',
   },
+  ui_splash_contain_globe_cadre_tags: {
+    borderRadius: 100,
+    width: 60,
+    height: 65,
+    backgroundColor: '#f44336',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   ui_splash_contain_destination_items_globe1: {
     flexDirection: 'row',
+  },
+  ui_splash_contain_destination_items_contains_globe2: {
+    width: '80%',
+    justifyContent: 'center',
+    marginLeft: 5,
   },
   ui_splash_contain_destination_items_globe2: {
     flexDirection: 'row',
   },
   ui_splash_contain_destination_items_text1: {
-    margin: 5,
+    marginLeft: 1,
     color: 'white',
     fontSize: 19,
   },
+  ui_splash_contain_destination_items_globe_text2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   ui_splash_contain_destination_items_text2: {
-    margin: 5,
+    marginLeft: 1,
+    marginRight: 10,
     color: '#e1e1e1',
   },
 });
