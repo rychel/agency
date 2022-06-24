62b963e6ba22f1cb5d2f60c38c36b0f59b2e01db
Maxence <maxrychel100@gmail.com>
2022-06-24 18:55:17 +0200

    Creating bus information component

---
+++ src/screens/Direction/Bus.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const Bus = (props) => {
+  
+  return (
+    <View style={styles.ui_splash_contain_header_globe}>
+        <Text>Ici ce sont les bus de noblesse voyages</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+
+});
+
+export default Bus;
