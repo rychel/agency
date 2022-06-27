949cc503a4f596e39981a8b39306970dc8fbde1c
Maxence <maxrychel100@gmail.com>
2022-06-27 19:17:49 +0200

    Create configuration screen bus

---
+++ src/screens/Direction/Busconfiguration.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+import ButtonAddingItems from '../../components/ButtonAddingItems';
+import TipsBus from '../../components/TipsBus';
+import NoItemStatus from '../../components/NoItemStatus';
+import ItemChauffeurTips from '../../components/ItemChauffeurTips';
+
+const Busconfiguration = (props) => {
+  return (
+    <View style={styles.ui_splash_contain_header_globe}>
+      <NoItemStatus
+        Titleico="user-slash"
+        Titlestatus="aucun chauffeur enregistré"
+      />
+      <ItemChauffeurTips Titleico='user' Titlename='Bopp' Titlesubname='Dylan ngaite' onDelete={() => {}} onSleep={() => {}} />
+      <ButtonAddingItems
+        titleico="plus"
+        titlebutton="ajouter"
+        borderrounds={50}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ui_splash_contain_header_globe: {
+    width: '100%',
+    height: Dimensions.get('window').height,
+    backgroundColor: '#f1bfbf14',
+  },
+});
+
+export default Busconfiguration;
