ad9ce89cd496fe2f83fc7f90d8dd845d800f604a
Maxence <maxrychel100@gmail.com>
2022-06-27 15:30:45 +0200

    create setting screen for direction route

---
+++ src/screens/Direction/Setting.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const Setting = (props) => {
+ 
+  return (
+    <View>
+        <Text>Gloire à Dieu</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default Setting;
