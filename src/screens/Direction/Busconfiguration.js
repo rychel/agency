7ee779a0e0b9aeab44db8433eaa761a6feeafc2f
Maxence <maxrychel100@gmail.com>
2022-06-30 06:26:00 +0200

    create Busconfiguration for demo Direction screen

--- src/screens/Direction/Busconfiguration.js
+++ src/screens/Direction/Busconfiguration.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   Dimensions,
+  StatusBar,
 } from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
@@ -12,20 +13,23 @@ import ButtonAddingItems from '../../components/ButtonAddingItems';
 import TipsBus from '../../components/TipsBus';
 import NoItemStatus from '../../components/NoItemStatus';
 import ItemChauffeurTips from '../../components/ItemChauffeurTips';
+import ItemBusTips from '../../components/ItemBusTips';
 
 const Busconfiguration = (props) => {
+
   return (
     <View style={styles.ui_splash_contain_header_globe}>
+      <StatusBar
+        animated={true}
+        backgroundColor="#7cc3bc"
+        barStyle="default"
+        showHideTransition="fade"
+        hidden={false} />
       <NoItemStatus
-        Titleico="user-slash"
-        Titlestatus="aucun chauffeur enregistré"
+        Titleico="tint-slash"
+        Titlestatus="aucun bus enregistré"
       />
-      <ItemChauffeurTips Titleico='user' Titlename='Bopp' Titlesubname='Dylan ngaite' onDelete={() => {}} onSleep={() => {}} />
-      <ButtonAddingItems
-        titleico="plus"
-        titlebutton="ajouter"
-        borderrounds={50}
-      />
+      <ItemBusTips Titleico='bus' Titlename='Bopp' Titlesubname='Dylan ngaite' onDelete={() => {}} onSleep={() => {}} />
     </View>
   );
 };
