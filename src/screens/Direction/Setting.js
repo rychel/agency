37938d89c129831010e67200625035c32426d0c5
Maxence <maxrychel100@gmail.com>
2022-06-30 06:39:43 +0200

    Add status bar to setting screen for Direction

--- src/screens/Direction/Setting.js
+++ src/screens/Direction/Setting.js
@@ -4,22 +4,43 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  Dimensions,
-  Image,
+  StatusBar,
 } from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-const Setting = (props) => {
- 
+/* import utils */
+import ItemMenu from '../../components/ItemMenu';
+
+const Setting = props => {
   return (
     <View>
-        <Text>Gloire à Dieu</Text>
+      <StatusBar
+        animated={true}
+        backgroundColor="#7cc3bc"
+        barStyle="default"
+        showHideTransition="fade"
+        hidden={false}
+      />
+      <ItemMenu Titleico="chart-pie" Titlename="Soucrire à un forfait" />
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.navigate('Chauffeur');
+        }}
+        activeOpacity={0.7}>
+        <ItemMenu Titleico="users" Titlename="Gérer les chauffeurs" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.navigate('Busconfiguration');
+        }}
+        activeOpacity={0.7}>
+        <ItemMenu Titleico="bus-alt" Titlename="Gérer les Bus" />
+      </TouchableOpacity>
+      <ItemMenu Titleico="receipt" Titlename="Choisir le type de ticket" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
 
 export default Setting;
