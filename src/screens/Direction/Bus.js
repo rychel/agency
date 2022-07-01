22a72e495d3c96146d8834be96d68aeeb6993f3c
Maxence <maxrychel100@gmail.com>
2022-07-01 07:27:19 +0200

    Add direction demo screen for Direction

--- src/screens/Direction/Bus.js
+++ src/screens/Direction/Bus.js
@@ -4,22 +4,40 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
+  Dimensions,
 } from 'react-native';
-
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+import ButtonAddingItems from '../../components/ButtonAddingItems';
+import TipsBus from '../../components/TipsBus';
 
 const Bus = (props) => {
   
   return (
     <View style={styles.ui_splash_contain_header_globe}>
-        <Text>Ici ce sont les bus de noblesse voyages</Text>
+        <TipsBus Idbus={676} 
+                 Immatbus='JK76JK76' 
+                 Chauffeurbus='Bopp' 
+                 Typebus='Coaster' />
+        <TipsBus Idbus={766}
+                 Immatbus='K7840976' 
+                 Chauffeurbus='Chris' 
+                 Typebus='Vip' />
+        <TipsBus Idbus={226} 
+                 Immatbus='M7SFD976' 
+                 Chauffeurbus='Bartholomey' 
+                 Typebus='Vip' />
+        <ButtonAddingItems titleico='plus' titlebutton='ajouter' borderrounds={50} />
     </View>
-  );
+  )
 };
 
 const styles = StyleSheet.create({
-
-
+  ui_splash_contain_header_globe: {
+    width: '100%',
+    height: Dimensions.get('window').height,
+    backgroundColor: '#f1bfbf14',
+  }
 });
 
 export default Bus;
