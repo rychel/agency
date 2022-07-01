a4046b1c3036fc667111f3c3fef96c4de0dada79
Maxence <maxrychel100@gmail.com>
2022-07-01 07:32:08 +0200

    Add Profil user tips for Direction

--- src/components/ProfilUser.js
+++ src/components/ProfilUser.js
@@ -1,71 +1,72 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-const ProfilUser = (props) => {
+const ProfilUser = props => {
   const {Utitle, Username, Uphoto, Ustatut} = props;
-  
+
   return (
     <TouchableOpacity
-            style={styles.ui_splash_started_button_welcome}
-            activeOpacity={0.7}>
-    <View style={styles.ui_splash_contain_profil_globe}>
-      
-      <View style={styles.ui_splash_contain_profil_cadre_globe}>
-        <Image
+      activeOpacity={0.7}>
+      <View style={styles.ui_splash_contain_profil_globe}>
+        <View style={styles.ui_splash_contain_profil_cadre_globe}>
+          {/*Image time zone*/}
+          <Image
             source={Uphoto}
             resizeMode="cover"
             style={styles.ui_splash_contain_profil_cadre_photo}
-        />
+          />
+        </View>
+        {/*Status time connect*/}
+        <Text
+          style={[
+            styles.ui_splash_contain_profil_cadre_title_icon_connexion,
+            {backgroundColor: Ustatut},
+          ]}></Text>
+        {/*Name of post*/}
+        <Text>
+          {Utitle} : {Username}
+        </Text>
       </View>
-      <Text style={[styles.ui_splash_contain_profil_cadre_title_icon_connexion, {backgroundColor: Ustatut}]}></Text>
-      <Text>{Utitle} : {Username}</Text>
-    </View>      
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-    ui_splash_contain_profil_globe: {
-        width: 160,
-        height: 172,
-        borderWidth: 1.6,
-        borderColor: '#83838321',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 2,
-        borderTopWidth: 0,
-        borderBottomColor: 0,
-        borderLeftWidth: 0,
-    },
-    ui_splash_contain_profil_cadre_globe: {
-        width: 120,
-        height: 120,
-        borderRadius: 100,
-        margin: 2,
-        overflow: 'hidden',
-    },
-    ui_splash_contain_profil_cadre_title_icon_connexion: {
-      position: 'relative',
-      top: -10,
-      fontSize: 19,
-      width: 20,
-      height: 20,
-      borderRadius: 100,
-    },
-    ui_splash_contain_profil_cadre_photo: {
-      alignSelf: 'center',
-      width: 120,
-      height: 120,
-      borderRadius: 100,
-    }
+  ui_splash_contain_profil_globe: {
+    width: 125,
+    height: 172,
+    borderWidth: 1.6,
+    borderColor: '#83838321',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 2,
+    borderTopWidth: 0,
+    borderBottomColor: 0,
+    borderLeftWidth: 0,
+  },
+  ui_splash_contain_profil_cadre_globe: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    margin: 2,
+    overflow: 'hidden',
+  },
+  ui_splash_contain_profil_cadre_title_icon_connexion: {
+    position: 'relative',
+    top: -10,
+    fontSize: 19,
+    width: 20,
+    height: 20,
+    borderRadius: 100,
+  },
+  ui_splash_contain_profil_cadre_photo: {
+    alignSelf: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+  },
 });
 
 export default ProfilUser;
