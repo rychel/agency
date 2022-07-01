e5573fb8d1ebfe70925c0a796b1b0d927c34d73b
Maxence <maxrychel100@gmail.com>
2022-07-01 09:49:53 +0200

    Add button adding of trajet for Trajet screen for Direction

--- src/screens/Direction/Trajet.js
+++ src/screens/Direction/Trajet.js
@@ -11,6 +11,7 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import NotificationExplain from '../../components/NotificationExplain';
 import ItemConfigSlug from '../../components/ItemConfigSlug';
+import ButtonAddingItems from '../../components/ButtonAddingItems';
 
 const Trajet = (props) => {
 
@@ -22,10 +23,11 @@ const Trajet = (props) => {
         barStyle="default"
         showHideTransition="fade"
         hidden={false} />
-      <NotificationExplain Titlemessage="Vous pouvez établir tous les trajets fait par votre agence, les destinations principales ainsi que les destinations secondaires (zones situées entre la ville de départ et celle d'arrivée )" />
+      <NotificationExplain Titlemessage="Vous pouvez établir tous les trajets fait par votre agence, les destinations principales ainsi que les destinations secondaires (zones situées entre la ville de départ et celle d'arrivée)" />
      
       <ItemConfigSlug Titlechoice='Ticket' Titlemessage='Les clients de votre agence auront un ticket physique et qui sera utilisé par le composteur.
                 Les numéros de ticket seront fournis par le système.' />
+      <ButtonAddingItems titleico='plus' titlebutton='ajouter' borderrounds={50} />
     </View>
   );
 };
