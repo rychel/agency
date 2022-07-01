d1c2c7b35448d00a8059ebc50c12f01692c0df45
Maxence <maxrychel100@gmail.com>
2022-07-01 09:16:02 +0200

    Create transport screen for Direction

---
+++ src/screens/Direction/Transport.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+
+
+import ItemMenu from '../../components/ItemMenu';
+
+const Transport = (props) => {
+  return (
+    <View>
+      <StatusBar
+        animated={true}
+        backgroundColor="#7cc3bc"
+        barStyle="default"
+        showHideTransition="fade"
+        hidden={false}
+      />
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.navigate('Chauffeur');
+        }}
+        activeOpacity={0.7}>
+        <ItemMenu Titleico="users" Titlename="Gérer les chauffeurs" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.navigate('Busconfiguration');
+        }}
+        activeOpacity={0.7}>
+        <ItemMenu Titleico="bus-alt" Titlename="Gérer les Bus" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.navigate('Ticketconfiguration');
+        }}
+        activeOpacity={0.7}>
+        <ItemMenu Titleico="receipt" Titlename="Choisir le type de ticket" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Transport;
