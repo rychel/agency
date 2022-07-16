c63e4064ed8bd9308ba51676007a41dc3c807d2b
Maxence <maxrychel100@gmail.com>
2022-07-16 03:48:24 +0200

    Update HeaderUser bar to v0.0.1 acxios

--- src/components/HeaderUser.js
+++ src/components/HeaderUser.js
@@ -3,58 +3,40 @@ import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-const HeaderUser = props => {
+const HeaderUser = (props) => {
   const {Gtitle, Depart, Arrive, onPress} = props;
 
   return (
     <View style={styles.ui_splash_contain_header_globe}>
       <View style={styles.ui_splash_contain_destination_items_globe_text2}>
-        <TouchableOpacity activeOpacity={0.7} onPress={onPress}>
-          <View style={styles.ui_splash_contain_globe_cadre_tags}>
-            <FontAwesome5 name="align-justify" size={30} color="white" />
-          </View>
+        <TouchableOpacity activeOpacity={0.7} onPress={onPress} style={styles.ui_splash_contain_globe_cadre_tags}>
+          <FontAwesome5 name="align-justify" size={28} color="white" />
         </TouchableOpacity>
       </View>
       <View style={styles.ui_splash_contain_destination_items_contains_globe2}>
-        <View style={styles.ui_splash_contain_destination_items_globe2}>
-          <Text style={styles.ui_splash_contain_destination_items_text1}>
-            {Gtitle}
-          </Text>
-        </View>
         <View style={styles.ui_splash_contain_destination_globe}>
-          <View style={styles.ui_splash_contain_destination_items_globe1}>
-            <Text style={styles.ui_splash_contain_destination_items_text2}>
-              agence
-            </Text>
-            <Text style={styles.ui_splash_contain_destination_items_text2}>
-              {Depart}-{Arrive}
-            </Text>
-          </View>
+          <Text style={styles.ui_splash_contain_destination_items_text1}>{Gtitle}</Text>
+          <Text style={styles.ui_splash_contain_destination_items_text2}>{Depart}-{Arrive}</Text>
         </View>
       </View>
       <View style={styles.ui_splash_contain_destination_items_globe_text2}>
         <Text style={styles.ui_splash_contain_destination_items_text2}>
-          <FontAwesome5 name="search" size={24} color="white" />
+          <FontAwesome5 name="search" size={22} color="white" />
         </Text>
       </View>
     </View>
   );
 };
 
-{
-  /*#f44336 */
-}
-
 const styles = StyleSheet.create({
   ui_splash_contain_header_globe: {
     backgroundColor: '#7cc3bc',
     flexDirection: 'row',
-    height: 70,
+    height: 50,
     justifyContent: 'center',
   },
   ui_splash_contain_destination_globe: {
-    flexDirection: 'row',
-    backgroundColor: '#7cc3bc',
+    flexDirection: 'column',
     justifyContent: 'space-between',
   },
   ui_splash_contain_globe_cadre_tags: {
@@ -65,21 +47,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  ui_splash_contain_destination_items_globe1: {
-    flexDirection: 'row',
-  },
   ui_splash_contain_destination_items_contains_globe2: {
-    width: '80%',
+    width: '70%',
     justifyContent: 'center',
     marginLeft: 5,
   },
-  ui_splash_contain_destination_items_globe2: {
-    flexDirection: 'row',
-  },
   ui_splash_contain_destination_items_text1: {
-    marginLeft: 1,
     color: 'white',
-    fontSize: 19,
+    fontSize: 18,
+    fontFamily: 'Foundation',
+    padding: 2,
   },
   ui_splash_contain_destination_items_globe_text2: {
     justifyContent: 'center',
@@ -88,7 +65,7 @@ const styles = StyleSheet.create({
   ui_splash_contain_destination_items_text2: {
     marginLeft: 1,
     marginRight: 10,
-    color: '#e1e1e1',
+    color: '#bbaa12',
   },
 });
 
