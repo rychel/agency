5ee5adbc71429a62720bdc14704c9c701dde30cc
Maxence <maxrychel100@gmail.com>
2022-07-18 13:12:24 +0200

    Create ItemStatDirection tips to v0.0.1 acxios

---
+++ src/components/ItemStatDirection.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const ItemStatDirection = props => {
+  const {Titleico, Titlestat, Titlesub, Titlebg, Titlecl} = props;
+
+  return (
+    <View style={[styles.ui_splash_contain_header_globe, {backgroundColor: Titlebg}]}>
+      <FontAwesome5 name={Titleico} size={20} color="white" />
+      <Text style={[styles.ui_splash_contain_header_params_fonts4, {color: Titlecl}]}>{Titlestat}</Text>
+      <Text style={styles.ui_splash_contain_header_params_fonts5}>{Titlesub}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ui_splash_contain_header_globe: {
+    width: '22.33%',
+    height: 100,
+    margin: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+  },
+  ui_splash_contain_header_params_fonts4: {
+    fontWeight: '800',
+    fontSize: 20,
+    color: '#f44336',
+  },
+  ui_splash_contain_header_params_fonts5: {
+    fontSize: 11,
+    fontFamily: 'Nunito-Regular',
+  },
+});
+
+export default ItemStatDirection;
