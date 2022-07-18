3262baee38a2b499e0309d94a8a8d3b4144caa85
Maxence <maxrychel100@gmail.com>
2022-07-18 12:36:18 +0200

    Replace stats agency to principal Direction to v0.0.1 acxios

--- src/components/DrawerDirection.js
+++ src/components/DrawerDirection.js
@@ -28,62 +28,6 @@ const DrawerDirection = props => {
         Titlenumber="673845359"
       />
 
-      <View style={styles.ui_splash_contain_header_tab_stats_container}>
-        <View style={styles.ui_splash_contain_header_tab_stats_contain}>
-          <View style={styles.ui_splash_contain_header_items_contains}>
-            <Text style={styles.ui_splash_contain_header_params_fonts4}>
-              30
-            </Text>
-            <Text style={styles.ui_splash_contain_header_params_fonts5}>
-              Chauffeur
-            </Text>
-          </View>
-          <View style={styles.ui_splash_contain_header_items_contains}>
-            <Text style={styles.ui_splash_contain_header_params_fonts4}>
-              40
-            </Text>
-            <Text style={styles.ui_splash_contain_header_params_fonts5}>
-              Coaster
-            </Text>
-          </View>
-          <View style={styles.ui_splash_contain_header_items_contains}>
-            <Text style={styles.ui_splash_contain_header_params_fonts4}>3</Text>
-            <Text style={styles.ui_splash_contain_header_params_fonts5}>
-              Vip
-            </Text>
-          </View>
-          <View style={styles.ui_splash_contain_header_items_contains}>
-            <Text style={styles.ui_splash_contain_header_params_fonts4}>
-              20
-            </Text>
-            <Text style={styles.ui_splash_contain_header_params_fonts5}>
-              Porteur
-            </Text>
-          </View>
-        </View>
-
-        <View style={styles.ui_splash_contain_header_tab_stats_contain}>
-          <View style={styles.ui_splash_contain_header_items_contains}>
-            <Text style={styles.ui_splash_contain_header_params_fonts4}>2</Text>
-            <Text style={styles.ui_splash_contain_header_params_fonts5}>
-              Caissière
-            </Text>
-          </View>
-          <View style={styles.ui_splash_contain_header_items_contains}>
-            <Text style={styles.ui_splash_contain_header_params_fonts4}>1</Text>
-            <Text style={styles.ui_splash_contain_header_params_fonts5}>
-              Sécrétaire
-            </Text>
-          </View>
-          <View style={styles.ui_splash_contain_header_items_contains}>
-            <Text style={styles.ui_splash_contain_header_params_fonts4}>2</Text>
-            <Text style={styles.ui_splash_contain_header_params_fonts5}>
-              Composteur
-            </Text>
-          </View>
-        </View>
-      </View>
-
       {/*administration session*/}
       <View
         style={styles.ui_splash_contain_header_administration_contains_block}>
@@ -124,37 +68,9 @@ const styles = StyleSheet.create({
   ui_splash_contain_header_params_fonts5: {
     fontSize: 11,
   },
-  ui_splash_contain_header_tab_stats_container: {
-    width: '100%',
-    height: 220,
-    borderRadius: 5,
-    flexDirection: 'column',
-    borderTopWidth: 0,
-    borderRightWidth: 0,
-    borderLeftWidth: 0,
-    borderWidth: 1,
-    borderColor: '#03a9f41a',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  ui_splash_contain_header_tab_stats_contain: {
-    width: '100%',
-    height: 70,
-    top: 2,
-    flexDirection: 'row',
-    marginBottom: 20,
-  },
-  ui_splash_contain_header_items_contains: {
-    width: '22.33%',
-    height: 80,
-    margin: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 100,
-  },
   ui_splash_contain_header_administration_contains_block: {
     width: '100%',
+    height: 310,
     backgroundColor: 'white',
   },
 });
