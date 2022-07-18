5a036db93dcfb53d24b64f22b481bfd61aa74663
Maxence <maxrychel100@gmail.com>
2022-07-18 12:02:13 +0200

    Update DashboardDirection to v0.0.1 acxios

--- src/components/DashboardDirection.js
+++ src/components/DashboardDirection.js
@@ -12,7 +12,7 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const MiniMenuDirection = (props) => {
   const {Titleico, Titleparam, onClose} = props;
-
+  {/*#0a0a0aba */}
   return (
     <View style={styles.ui_splash_contain_header_globe}>
       <TouchableOpacity
@@ -24,7 +24,7 @@ const MiniMenuDirection = (props) => {
         <TouchableOpacity
           style={styles.ui_splash_contain_item_config_param}
           activeOpacity={0.6}>
-          <FontAwesome5 name="calendar-check" color="#ffc107" size={20} />
+          <FontAwesome5 name="calendar-check" color="#ffc107" size={25} />
           <Text style={styles.ui_splash_contain_config_fonts1}>
             Tableau de bord du jour
           </Text>
@@ -50,9 +50,6 @@ const styles = StyleSheet.create({
     right: 5,
     bottom: 0,
     marginBottom: 80,
-    borderWidth: 0.5,
-    borderColor: 'white',
-    backgroundColor: 'white',
     borderRadius: 5,
   },
   ui_splash_contain_item_config_button_back: {
@@ -72,7 +69,8 @@ const styles = StyleSheet.create({
   },
   ui_splash_contain_config_fonts1: {
     fontSize: 16,
-    color: '#0a0a0aba',
+    color: 'white',
+    top: 4,
   },
 });
 
