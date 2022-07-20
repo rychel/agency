c9e46220bf9b16e780d1b18338063129178474a3
Maxence <maxrychel100@gmail.com>
2022-07-21 01:57:51 +0200

    Add Uphoto props of ProfilUser to v0.0.1 acxios

--- src/components/ProfilUser.js
+++ src/components/ProfilUser.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-
 const ProfilUser = props => {
-  const {Utitle, Username, Ustatut} = props;
+  const {Utitle, Username, Ustatut, Uphoto} = props;
 
   return (
     <TouchableOpacity activeOpacity={0.7}>
       <View style={styles.ui_splash_contain_profil_globe}>
+        <Image
+          source={Uphoto}
+          style={styles.ui_splash_contain_profil_cadre_logo}
+        />
         <View style={styles.ui_splash_contain_profil_cadre_globe}>
           <Text style={styles.ui_splash_config_fonts1}>{Username}</Text>
         </View>
@@ -25,8 +27,8 @@ const ProfilUser = props => {
 
 const styles = StyleSheet.create({
   ui_splash_contain_profil_globe: {
-    width: 85,
-    height: 92,
+    width: 115,
+    height: 150,
     borderWidth: 1.6,
     borderColor: '#f7f5f5cc',
     justifyContent: 'center',
@@ -35,29 +37,39 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderBottomColor: 0,
     borderLeftWidth: 0,
+    flexDirection: 'column',
   },
   ui_splash_contain_profil_cadre_globe: {
     width: 50,
     height: 50,
     borderRadius: 100,
-    margin: 2,
     overflow: 'hidden',
-    backgroundColor: '#03a9f49e',
+    backgroundColor: '#03a9f4',
     alignItems: 'center',
     justifyContent: 'center',
+    top: -15,
+    zIndex: 2,
   },
   ui_splash_contain_profil_cadre_title_icon_connexion: {
     position: 'relative',
-    top: -10,
+    top: -22,
     fontSize: 19,
     width: 12,
     height: 12,
     borderRadius: 100,
+    zIndex: 3,
   },
   ui_splash_contain_profil_cadre_photo: {
     width: 100,
     height: 100,
     borderRadius: 100,
+  },
+  ui_splash_contain_profil_cadre_logo: {
+    width: 90,
+    height: 90,
+    borderRadius: 100,
+    zIndex: 1,
+    top: 14,
   },
   ui_splash_config_fonts1: {
     fontSize: 18,
@@ -67,6 +79,8 @@ const styles = StyleSheet.create({
   ui_splash_config_fonts2: {
     fontSize: 13,
     fontFamily: 'PontanoSans-Regular',
+    top: -15,
+    color: 'black',
   },
 });
 
