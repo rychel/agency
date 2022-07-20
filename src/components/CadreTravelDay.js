7a769a63114a56bd8fb6764e9c00a5467bc9c6a4
Maxence <maxrychel100@gmail.com>
2022-07-20 23:10:36 +0200

    Add setting button and change globaly of CadreTavelDay to v0.0.1 acxios

--- src/components/CadreTravelDay.js
+++ src/components/CadreTravelDay.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const CadreTravelDay = props => {
@@ -15,43 +15,53 @@ const CadreTravelDay = props => {
 
   return (
     <View style={styles.ui_splash_global_contain_global}>
-      <View style={styles.ui_splash_global_contain_first_info_ticket}>
-        <View style={styles.ui_splash_global_contain_first_info_cadre_logo}>
-          <Text style={styles.ui_splash_global_contain_config_fonts4}>
-            {Titleidbus}
-          </Text>
-          <FontAwesome5 name={Titlebusico} size={55} color="#000000b3" />
-          <Text style={styles.ui_splash_global_contain_config_fonts4}>
-            Porteur
-          </Text>
+      <View style={styles.ui_splash_global_contain_second_info_cadre_logo}>
+        <View style={styles.ui_splash_global_contain_first_info_ticket}>
+          <View style={styles.ui_splash_global_contain_first_info_cadre_logo}>
+            <Text style={styles.ui_splash_global_contain_config_fonts4}>
+              {Titleidbus}
+            </Text>
+            <FontAwesome5 name={Titlebusico} size={55} color="#000000b3" />
+            <Text style={styles.ui_splash_global_contain_config_fonts4}>
+              Porteur
+            </Text>
+          </View>
+        </View>
+        <View style={styles.ui_splash_global_contain_first_info_travel}>
+          <View style={styles.ui_splash_global_contain_wrapped_info_ticket}>
+            <View style={styles.ui_splash_global_contain_first_info_item}>
+              <Text style={styles.ui_splash_global_contain_config_fonts2}>
+                Départ: {Titledepart}-{Titledestination}
+              </Text>
+            </View>
+            <View style={styles.ui_splash_global_contain_first_info_item}>
+              <Text style={styles.ui_splash_global_contain_config_fonts2}>
+                Validation: {Titlemodevalidation}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.ui_splash_global_contain_wrapped_info_ticket}>
+            <View style={styles.ui_splash_global_contain_first_info_item}>
+              <Text style={styles.ui_splash_global_contain_config_fonts2}>
+                Ticket: {Titleprice}
+              </Text>
+            </View>
+            <View style={styles.ui_splash_global_contain_first_info_item}>
+              <Text style={styles.ui_splash_global_contain_config_fonts2}>
+                Heure: {Titleheure}
+              </Text>
+            </View>
+          </View>
         </View>
       </View>
-      <View style={styles.ui_splash_global_contain_first_info_travel}>
-        <View style={styles.ui_splash_global_contain_wrapped_info_ticket}>
-          <View style={styles.ui_splash_global_contain_first_info_item}>
-            <Text style={styles.ui_splash_global_contain_config_fonts2}>
-              Départ: {Titledepart}-{Titledestination}
-            </Text>
-          </View>
-          <View style={styles.ui_splash_global_contain_first_info_item}>
-            <Text style={styles.ui_splash_global_contain_config_fonts2}>
-              Validation: {Titlemodevalidation}
-            </Text>
-          </View>
-        </View>
-        <View style={styles.ui_splash_global_contain_wrapped_info_ticket}>
-          <View style={styles.ui_splash_global_contain_first_info_item}>
-            <Text style={styles.ui_splash_global_contain_config_fonts2}>
-              Ticket: {Titleprice}
-            </Text>
-          </View>
-          <View style={styles.ui_splash_global_contain_first_info_item}>
-            <Text style={styles.ui_splash_global_contain_config_fonts2}>
-              Heure: {Titleheure}
-            </Text>
-          </View>
-        </View>
-      </View>
+      <TouchableOpacity
+        style={styles.ui_splash_global_contain_third_info_cadre_logo}
+        activeOpacity={0.5}>
+        <Text style={styles.ui_splash_global_contain_config_fonts5}>
+          Modifier
+        </Text>
+        <FontAwesome5 name="ellipsis-h" size={30} color="#000000b3" />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -60,12 +70,12 @@ const styles = StyleSheet.create({
   ui_splash_global_contain_global: {
     margin: 5,
     width: 320,
-    height: 170,
-    flexDirection: 'row',
-    borderRadius: 10,
-    backgroundColor: '#f44336',
+    height: 210,
+    flexDirection: 'column',
+    borderRadius: 7,
     justifyContent: 'center',
     alignItems: 'center',
+    borderWidth: 0.2,
   },
   ui_splash_global_contain_first_info_travel: {
     width: 200,
@@ -86,12 +96,12 @@ const styles = StyleSheet.create({
     paddingBottom: 2,
   },
   ui_splash_global_contain_config_fonts2: {
-    fontFamily: 'Fontisto',
-    color: '#000000c7',
+    fontFamily: 'Feather',
+    color: '#f44336',
     padding: 3,
     borderRadius: 5,
-    backgroundColor: 'white',
-    height: 27,
+    backgroundColor: '#ddd7d730',
+    height: 23,
     textAlignVertical: 'center',
     fontSize: 13,
   },
@@ -109,7 +119,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Fontisto',
     color: '#000000ad',
     fontSize: 12,
-    left: 10,
+    left: -4,
   },
   ui_splash_global_contain_first_info_ticket: {
     margin: 3,
@@ -133,19 +143,22 @@ const styles = StyleSheet.create({
     margin: 3,
     alignItems: 'center',
     flexDirection: 'row',
-    width: '9%',
   },
   ui_splash_global_contain_third_info_cadre_logo: {
-    margin: 3,
     alignItems: 'center',
     flexDirection: 'row',
-    width: '90%',
+    width: 100,
+    height: 50,
+    backgroundColor: 'yellow',
+    borderRadius: 100,
+    top: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   ui_splash_global_contain_wrapped_info_ticket: {
     margin: 3,
     flexDirection: 'column',
     width: '90%',
-    backgroundColor: '#f44336',
   },
 });
 
