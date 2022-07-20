42e76d90a7bc6e140bc2b30e2dce19c59fbd9f24
Maxence <maxrychel100@gmail.com>
2022-07-20 07:27:42 +0200

    Add props bgcolor and Update TitleTravelDay to v0.0.1 acxios

--- src/components/TitleTravelDay.js
+++ src/components/TitleTravelDay.js
@@ -2,30 +2,55 @@ import React, {useState, useEffect, useRef} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-const TitleTravelDay = (props) => {
-  const {Titleico, Titlesub} = props;
+const TitleTravelDay = props => {
+  const {Titleico, Titlesub, Titlebg} = props;
 
   return (
-    <View
-      style={styles.ui_splash_global_contain_registered_global_contain_title}>
-      <Text
-        style={styles.ui_splash_global_contain_registered_global_config_font1}>
-        {Titlesub}
-      </Text>
-      <FontAwesome5 name={Titleico} size='20' color='blue' />
+    <View style={styles.ui_splash_global_contain_global}>
+      <View
+        style={[styles.ui_splash_global_contain_registered_global_contain_title, {backgroundColor: Titlebg}]}>
+        <Text
+          style={
+            styles.ui_splash_global_contain_registered_global_config_font1
+          }>
+          {Titlesub}
+        </Text>
+      </View>
+      <FontAwesome5
+        name={Titleico}
+        size={20}
+        color="#0061ab73"
+        style={styles.ui_splash_global_contain_registered_logo_config}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  ui_splash_global_contain_global: {
+    margin: 5,
+    flexDirection: 'row',
+    height: 42,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   ui_splash_global_contain_registered_global_contain_title: {
     margin: 5,
     flexDirection: 'row',
-    width: 200,
-    backgroundColor: 'blue',
+    height: 35,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5,
   },
   ui_splash_global_contain_registered_global_config_font1: {
     fontFamily: 'PingFang SC Regular',
+    fontSize: 15,
+    color: '#000000b8',
+  },
+  ui_splash_global_contain_registered_logo_config: {
+    transform: [{rotateZ: '40deg'}],
+    marginRight: 5,
   },
 });
 
