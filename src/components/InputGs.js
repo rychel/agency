c133c92671aef1ce515cb5281feb6aa9cc8463f8
Maxence <maxrychel100@gmail.com>
2022-07-21 08:28:14 +0200

    Update InputGs to v0.0.1 acxios

--- src/components/InputGs.js
+++ src/components/InputGs.js
@@ -1,69 +1,69 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-} from 'react-native';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
 
-const InputGs = (props) => {
-  const {title, keyboard, onChange, onBlur, onChangeText, errors, onFocus} = props;
-  
+const InputGs = props => {
+  const {title, keyboard, onChange, onBlur, onChangeText, errors, onFocus} =
+    props;
+
   return (
-    <View>
+    <View style={styles.ui_splash_global_control}>
       <Text style={styles.ui_splash_title_form_control}>{title}</Text>
       <TextInput
-          style={errors ? styles.ui_splash_input_form_control_error : styles.ui_splash_input_form_control}
-          keyboardType={keyboard}
-          onChange={onChange}
-          onBlur={onBlur}
-          onChangeText={onChangeText}
-          errors={errors}
-          onFocus={onFocus}
-          {...props}
+        style={
+          errors
+            ? styles.ui_splash_input_form_control_error
+            : styles.ui_splash_input_form_control
+        }
+        keyboardType={keyboard}
+        onChange={onChange}
+        onBlur={onBlur}
+        onChangeText={onChangeText}
+        errors={errors}
+        onFocus={onFocus}
+        {...props}
       />
-      {errors ? <Text style={styles.ui_splash_title_form_error_control}>{errors}</Text> : null}
+      {errors ? (
+        <Text style={styles.ui_splash_title_form_error_control}>{errors}</Text>
+      ) : null}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  ui_splash_global_control: {
+    
+  },
   ui_splash_input_form_control: {
     width: '90%',
-    marginTop: 12,
-    height: 39,
+    height: 34,
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 0.6,
-    borderColor: '#A3BDED',
+    borderColor: '#00000036',
     fontSize: 15,
+    color: 'orange',
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth: 0,
-    color: 'orange',
+    marginBottom: 1,
+    left: -4,
   },
   ui_splash_input_form_control_error: {
     width: '90%',
-    marginTop: 8,
-    marginBottom: 10,
-    height: 50,
-    alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 5,
-    borderWidth: 1.6,
+    height: 34,
+    borderWidth: 0.6,
     borderColor: 'red',
-    fontSize: 17,
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth: 0,
     color: 'orange',
+    left: 12,
   },
   ui_splash_title_form_control: {
     left: 16,
     fontSize: 15,
-    top: 22,
+    top: 5,
     color: '#A3BDED',
     fontFamily: 'PatrickHandSC-Regular',
   },
