adb01aea073c7d1c28f5e7b859c3be9fb3e14638
Maxence <maxrychel100@gmail.com>
2022-07-21 20:05:04 +0200

    Add show and mask props for deconnexion zone of DrawerDirection to v0.0.1 acxios

--- src/components/DrawerDirection.js
+++ src/components/DrawerDirection.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
-  Text,
   StyleSheet,
-  TouchableOpacity,
   Dimensions,
-  Image,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import {
-  DrawerContentScrollView,
-  DrawerItemList,
-} from '@react-navigation/drawer';
+import {DrawerContentScrollView} from '@react-navigation/drawer';
 
 import PersonalProfil from './PersonalProfil';
 import ItemDrawerMenu from './ItemDrawerMenu';
 
-const DrawerDirection = (props) => {
+const DrawerDirection = props => {
+  const [signout, setSignout] = useState(false);
+
   return (
     <DrawerContentScrollView style={styles.ui_splash_contain_header_globe}>
       <PersonalProfil
@@ -25,9 +23,28 @@ const DrawerDirection = (props) => {
         Titlesubname="Parfait Simb Coig"
         Titlepost="Chef d'agence"
         Titlenumber="673845359"
+        onOpen={() => setSignout(false)}
+        onClose={() => setSignout(true)}
       />
       <View
         style={styles.ui_splash_contain_header_administration_contains_block}>
+        {signout ? (
+          <View style={styles.ui_splash_contain_header_deconnexion_contains}>
+            <TouchableOpacity
+              style={styles.ui_splash_contain_header_deconnexion_contains_block}
+              activeOpacity={0.6}>
+              <FontAwesome5
+                name="door-open"
+                size={17}
+                color="white"
+                style={styles.ui_splash_contain_log_sign_configs1}
+              />
+              <Text style={styles.ui_splash_contain_text_configs1}>
+                Se déconnecter
+              </Text>
+            </TouchableOpacity>
+          </View>
+        ) : null}
         <ItemDrawerMenu
           Titleico="user-friends"
           Titlename="Créer un poste"
@@ -69,6 +86,28 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 310,
     backgroundColor: 'white',
+  },
+  ui_splash_contain_header_deconnexion_contains_block: {
+    width: 140,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  ui_splash_contain_header_deconnexion_contains: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#00bcd4',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  ui_splash_contain_log_sign_configs1: {
+    left: 5,
+  },
+  ui_splash_contain_text_configs1: {
+    left: 10,
+    fontFamily: 'materialcommunityIcons',
+    color: 'white',
   },
 });
 
