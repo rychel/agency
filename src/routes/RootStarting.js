2d0f9a9f9b183be5449279e1072ab3ca9a068d5a
Maxence <maxrychel100@gmail.com>
2022-07-21 15:06:31 +0200

    Add Getconnecting route to v0.0.1 acxios

--- src/routes/RootStarting.js
+++ src/routes/RootStarting.js
@@ -11,6 +11,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Launchnoaccount from '../screens/Root/Launchnoaccount';
 import Getstarting from '../screens/Root/Getstarting';
 import Grantstarting from '../screens/Root/Grantstarting';
+import Getconnecting from '../screens/Root/Getconnecting';
 
 const Stack = createStackNavigator();
 
@@ -30,6 +31,11 @@ const RootStarting = () => {
       <Stack.Screen
         name="admin"
         component={Grantstarting}
+        options={{header: () => null}}
+      />
+      <Stack.Screen
+        name="Connecting"
+        component={Getconnecting}
         options={{header: () => null}}
       />
     </Stack.Navigator>
