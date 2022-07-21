c52b56a73edf0f855412fda26700dfb490c5efcd
Maxence <maxrychel100@gmail.com>
2022-07-21 15:15:30 +0200

    Create InputGc component for connction fields to v0.0.1 acxios

---
+++ src/components/InputGc.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+
+const InputGc = props => {
+  const {title, keyboard, onChange, onBlur, onChangeText, errors, onFocus} =
+    props;
+
+  return (
+    <View style={styles.ui_splash_global_control}>
+      <Text style={styles.ui_splash_title_form_control}>{title}</Text>
+      <TextInput
+        style={
+          errors
+            ? styles.ui_splash_input_form_control_error
+            : styles.ui_splash_input_form_control
+        }
+        keyboardType={keyboard}
+        onChange={onChange}
+        onBlur={onBlur}
+        onChangeText={onChangeText}
+        errors={errors}
+        onFocus={onFocus}
+        {...props}
+      />
+      {errors ? (
+        <View style={styles.ui_splash_title_form_error_caps_control}>
+          <Text style={styles.ui_splash_title_form_error_control}>
+            {errors}
+          </Text>
+        </View>
+      ) : null}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ui_splash_input_form_control: {
+    width: '90%',
+    height: 37,
+    alignSelf: 'center',
+    borderWidth: 0.6,
+    borderColor: '#0a0a0a61',
+    fontSize: 15,
+    color: '#9c27b0',
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    marginBottom: 1,
+    left: -4,
+  },
+  ui_splash_input_form_control_error: {
+    width: '90%',
+    height: 37,
+    alignSelf: 'center',
+    borderWidth: 0.6,
+    borderColor: 'red',
+    fontSize: 15,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    color: '#9c27b0',
+    marginBottom: 1,
+    left: -4,
+  },
+  ui_splash_title_form_control: {
+    left: 16,
+    fontSize: 15,
+    top: 5,
+    color: '#0a0a0a9e',
+    fontFamily: 'opensanscondensedbold',
+  },
+  ui_splash_title_form_error_control: {
+    left: 14,
+    fontSize: 10.45,
+    color: 'red',
+    padding: 3,
+    backgroundColor: '#ffeb3b',
+    borderRadius: 100,
+  },
+  ui_splash_title_form_error_caps_control: {
+    flexDirection: 'row',
+  },
+});
+
+export default InputGc;
