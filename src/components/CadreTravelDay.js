5e198233593c72554b6009da1acf921f8aa546f9
Maxence <maxrychel100@gmail.com>
2022-07-29 07:13:14 +0200

    New update CadreTravelDay to v0.0.1 acxios

--- src/components/CadreTravelDay.js
+++ src/components/CadreTravelDay.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Animated} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEllipsisH} from '@fortawesome/free-solid-svg-icons';
 
 const CadreTravelDay = props => {
   const {
@@ -16,177 +16,130 @@ const CadreTravelDay = props => {
     Titlereste,
     Titleplace,
   } = props;
-  const StartWelcomeBrave = useRef(new Animated.Value(64)).current;
-
-  const ShowTitleidbus = () => {
-    Animated.sequence([
-      Animated.timing(StartWelcomeBrave, {
-        toValue: 25,
-        duration: 230,
-        useNativeDriver: true,
-      }),
-    ]).start();
-
-    setTimeout(() => {
-      HideTitleidbus();
-    }, 2000);
-  }
-
-  const HideTitleidbus = () => {
-    Animated.sequence([
-      Animated.timing(StartWelcomeBrave, {
-        toValue: 64,
-        duration: 230,
-        useNativeDriver: true,
-      }),
-    ]).start();
-  }
-
 
   return (
-    <TouchableOpacity
-      style={styles.ui_splash_global_contain_global}
-      onPressIn={ShowTitleidbus}
-      onPressOut={ShowTitleidbus}>
-      <View style={styles.ui_splash_global_contain_second_info_cadre_logo}>
-        <View style={styles.ui_splash_global_contain_first_info_ticket}>
-          <Animated.View
-            style={[
-              styles.ui_splash_global_contain_fouth_info_ticket,
-              {translateY: StartWelcomeBrave},
-            ]}>
-            <Text style={styles.ui_splash_global_contain_config_fonts9}>
-              {Titleidbus}
-            </Text>
-          </Animated.View>
-          <View style={styles.ui_splash_global_contain_first_info_cadre_logo}>
-            <FontAwesomeIcon icon={Titlebusico} size={55} color="#000000b3" />
-            <Text style={styles.ui_splash_global_contain_config_fonts4}>
-              {Titlebus}
-            </Text>
-          </View>
+    <View style={styles.ui_splash_global_contain_global}>
+      <View style={styles.ui_splash_global_contain_first_info_travel}>
+        <View style={styles.ui_splash_global_contain_logo_bus}>
+          <FontAwesomeIcon icon={Titlebusico} color="white" size={20} />
         </View>
-        <View style={styles.ui_splash_global_contain_first_info_travel}>
-          <View style={styles.ui_splash_global_contain_wrapped_info_ticket}>
-            <View style={styles.ui_splash_global_contain_first_info_item}>
-              <Text style={styles.ui_splash_global_contain_config_fonts3}>
-                Départ:
-              </Text>
-              <Text style={styles.ui_splash_global_contain_config_fonts2}>
-                {Titledepart}-{Titledestination}
-              </Text>
-            </View>
-            <View style={styles.ui_splash_global_contain_first_info_item}>
-              <Text style={styles.ui_splash_global_contain_config_fonts3}>
-                Validation:
-              </Text>
-              <Text style={styles.ui_splash_global_contain_config_fonts2}>
-                {Titlemodevalidation}
-              </Text>
-            </View>
-            <View style={styles.ui_splash_global_contain_first_info_item}>
-              <Text style={styles.ui_splash_global_contain_config_fonts3}>
-                Statut:
-              </Text>
-              <View style={styles.ui_splash_global_contain_second_info_item}>
-                <Text style={styles.ui_splash_global_contain_config_fonts6}>
-                  {Titlereste}(occupée)
-                </Text>
-                <Text style={styles.ui_splash_global_contain_config_fonts7}>
-                  /
-                </Text>
-                <Text style={styles.ui_splash_global_contain_config_fonts8}>
-                  {Titleplace}(reste)
-                </Text>
-              </View>
-            </View>
-          </View>
-          <View style={styles.ui_splash_global_contain_wrapped_info_ticket}>
-            <View style={styles.ui_splash_global_contain_first_info_item}>
-              <Text style={styles.ui_splash_global_contain_config_fonts3}>
-                Ticket:
-              </Text>
-              <Text style={styles.ui_splash_global_contain_config_fonts2}>
-                {Titleprice}
-              </Text>
-            </View>
-            <View style={styles.ui_splash_global_contain_first_info_item}>
-              <Text style={styles.ui_splash_global_contain_config_fonts3}>
-                Heure:
-              </Text>
-              <Text style={styles.ui_splash_global_contain_config_fonts2}>
-                {Titleheure}
-              </Text>
-            </View>
-          </View>
+        <View style={styles.ui_splash_global_contain_letter_title}>
+          <Text style={styles.ui_splash_global_contain_letter_title_bus}>
+            c
+          </Text>
+          <Text style={styles.ui_splash_global_contain_letter_title_bus}>
+            o
+          </Text>
+          <Text style={styles.ui_splash_global_contain_letter_title_bus}>
+            a
+          </Text>
+          <Text style={styles.ui_splash_global_contain_letter_title_bus}>
+            s
+          </Text>
+          <Text style={styles.ui_splash_global_contain_letter_title_bus}>
+            t
+          </Text>
+          <Text style={styles.ui_splash_global_contain_letter_title_bus}>
+            e
+          </Text>
+          <Text style={styles.ui_splash_global_contain_letter_title_bus}>
+            r
+          </Text>
         </View>
       </View>
-      <TouchableOpacity
-        style={styles.ui_splash_global_contain_third_info_cadre_logo}
-        activeOpacity={0.5}>
-        <FontAwesomeIcon icon={faEllipsisH} size={30} color="white" />
-      </TouchableOpacity>
-    </TouchableOpacity>
+      <View style={styles.ui_splash_global_contain_second_info_travel}>
+        <Text style={styles.ui_splash_global_contain_config_fonts2}>
+          Trajet:
+        </Text>
+        <Text style={styles.ui_splash_global_contain_config_fonts3}>
+          {Titledepart}-{Titledestination}
+        </Text>
+      </View>
+      <View style={styles.ui_splash_global_contain_second_info_travel}>
+        <Text style={styles.ui_splash_global_contain_config_fonts2}>
+          Ticket:
+        </Text>
+        <Text style={styles.ui_splash_global_contain_config_fonts3}>
+          {Titleprice}
+        </Text>
+      </View>
+      <View style={styles.ui_splash_global_contain_second_info_travel}>
+        <Text style={styles.ui_splash_global_contain_config_fonts2}>
+          Validation:
+        </Text>
+        <Text style={styles.ui_splash_global_contain_config_fonts3}>
+          {Titlemodevalidation}
+        </Text>
+      </View>
+      <View style={styles.ui_splash_global_contain_second_info_travel}>
+        <Text style={styles.ui_splash_global_contain_config_fonts1}>
+          Statut:
+        </Text>
+        <View style={styles.ui_splash_global_contain_second_info_item}>
+          <Text style={styles.ui_splash_global_contain_config_fonts6}>
+            {Titlereste}(occupée)
+          </Text>
+          <Text style={styles.ui_splash_global_contain_config_fonts7}>/</Text>
+          <Text style={styles.ui_splash_global_contain_config_fonts8}>
+            {Titleplace}(reste)
+          </Text>
+        </View>
+      </View>
+      <Text style={styles.ui_splash_global_contain_config_fonts4}>
+        Départ:{Titlereste}H
+      </Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   ui_splash_global_contain_global: {
     margin: 5,
-    width: 320,
+    width: 380,
     height: 210,
     flexDirection: 'column',
     borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#0061aba6',
+    borderWidth: 0.5,
+    borderColor: '#0000007a',
+    backgroundColor: '#8bc34a',
   },
   ui_splash_global_contain_first_info_travel: {
+    width: 380,
+    height: 40,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#cddc392b',
+  },
+  ui_splash_global_contain_letter_title: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    left: -5,
+  },
+  ui_splash_global_contain_letter_title_bus: {
+    width: 25,
+    height: 23,
+    justifyContent: 'space-between',
+    backgroundColor: '#ff9007',
+    borderRadius: 100,
+    fontSize: 18,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    color: 'white',
+  },
+  ui_splash_global_contain_logo_bus: {
+    width: 40,
+    height: 30,
+    justifyContent: 'center',
+    backgroundColor: '#8bc34a',
+    borderRadius: 100,
+    alignItems: 'center',
+    left: 5,
+  },
+  ui_splash_global_contain_five_info_travel: {
     width: 200,
     height: 160,
     justifyContent: 'space-between',
-  },
-  ui_splash_global_contain_first_info_item: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    margin: 2,
-    marginLeft: 2,
-    marginRight: 5,
-  },
-  ui_splash_global_contain_config_fonts1: {
-    width: 100,
-    fontFamily: 'Foundation',
-    color: '#00000094',
-    paddingBottom: 2,
-  },
-  ui_splash_global_contain_config_fonts2: {
-    color: 'white',
-    padding: 3,
-    borderRadius: 100,
-    backgroundColor: '#ffc107',
-    height: 23,
-    textAlignVertical: 'center',
-    fontSize: 13,
-  },
-  ui_splash_global_contain_config_fonts3: {
-    color: 'white',
-    padding: 3,
-    borderRadius: 5,
-    height: 23,
-    textAlignVertical: 'center',
-    fontSize: 12,
-    textTransform: 'uppercase',
-  },
-  ui_splash_global_contain_config_fonts4: {
-    fontFamily: 'Fontisto',
-    color: '#000000ad',
-    fontSize: 12,
-  },
-  ui_splash_global_contain_config_fonts5: {
-    fontFamily: 'Fontisto',
-    color: '#000000ad',
-    fontSize: 12,
-    left: -4,
   },
   ui_splash_global_contain_config_fonts6: {
     color: 'white',
@@ -196,6 +149,7 @@ const styles = StyleSheet.create({
     height: 23,
     textAlignVertical: 'center',
     fontSize: 10,
+    fontFamily: '',
   },
   ui_splash_global_contain_config_fonts7: {
     color: 'white',
@@ -205,77 +159,50 @@ const styles = StyleSheet.create({
     color: 'white',
     padding: 3,
     borderRadius: 100,
-    backgroundColor: '#8bc34a',
+    backgroundColor: '#cddc39',
     height: 23,
     textAlignVertical: 'center',
     fontSize: 10,
   },
-  ui_splash_global_contain_config_fonts9: {
-    color: '#000000d1',
-    padding: 5,
-    borderRadius: 2,
-    backgroundColor: 'white',
-    height: 23,
-    textAlignVertical: 'center',
-    fontSize: 13,
-    width: 80,
-    textAlign: 'center',
-    borderRadius: 5,
-  },
-  ui_splash_global_contain_first_info_ticket: {
-    margin: 3,
-    width: 110,
-    height: 160,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  ui_splash_global_contain_first_info_cadre_logo: {
-    margin: 3,
-    width: 100,
-    height: 98,
-    borderRadius: 15,
-    borderBottomEndRadius: 100,
-    borderBottomStartRadius: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  ui_splash_global_contain_second_info_cadre_logo: {
-    margin: 3,
+  ui_splash_global_contain_second_info_travel: {
+    width: 380,
+    height: 32,
+    justifyContent: 'space-around',
     alignItems: 'center',
     flexDirection: 'row',
-  },
-  ui_splash_global_contain_third_info_cadre_logo: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    width: 50,
-    height: 50,
-    backgroundColor: '#ffeb3b',
-    borderRadius: 100,
-    top: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  ui_splash_global_contain_wrapped_info_ticket: {
-    margin: 3,
-    flexDirection: 'column',
-    width: '90%',
+    backgroundColor: '#ffeb3b1f',
   },
   ui_splash_global_contain_second_info_item: {
-    flexDirection: 'row',
+    height: 90,
     justifyContent: 'space-between',
-    margin: 2,
-    marginLeft: 2,
-    marginRight: 5,
-  },
-  ui_splash_global_contain_fouth_info_ticket: {
-    margin: 3,
-    width: 60,
-    height: 60,
     alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 100,
-    transform: [{translateY: 68}],
+    flexDirection: 'row',
+  },
+  ui_splash_global_contain_config_fonts1: {
+    fontFamily: 'Nunito-ExtraLight',
+    fontSize: 17,
+    textTransform: 'uppercase',
+    color: 'white',
+  },
+  ui_splash_global_contain_config_fonts2: {
+    fontFamily: 'Nunito-ExtraLight',
+    fontSize: 15,
+    textTransform: 'uppercase',
+    color: 'white',
+  },
+  ui_splash_global_contain_config_fonts3: {
+    fontFamily: 'SairaCondensed-Thin',
+    fontSize: 14,
+    textTransform: 'uppercase',
+    color: 'white',
+  },
+  ui_splash_global_contain_config_fonts4: {
+    fontFamily: 'PontanoSans-Regular',
+    fontSize: 14,
+    color: 'white',
+    top: 20,
+    left: 2,
+    fontWeight: '800'
   },
 });
 
