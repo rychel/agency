737e98e4c0432a9e824dc7ca2d41354b1b6d4bf4
Maxence <maxrychel100@gmail.com>
2022-07-31 23:58:55 +0200

    Add LogOut action to v0.0.1 acxios

--- src/store/Log/actions.js
+++ src/store/Log/actions.js
@@ -2,4 +2,8 @@ export const Login = (Username, Password) => {
   const token = Username + Password;
   return {type: 'LOGIN', payload: token};
 };
+
+export const LogOut = () => {
+  return {type: 'LOGOUT', payload: null};
+};
  
