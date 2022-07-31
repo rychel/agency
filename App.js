8fa1a3e1b55b824871d29d7ee5b867864318f767
Maxence <maxrychel100@gmail.com>
2022-07-31 23:39:38 +0200

    Add Provider function for store global data to v0.0.1 acxios

--- App.js
+++ App.js
@@ -5,169 +5,38 @@
  * @format
  * @flow strict-local
  */
-
 import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
 import RNBootSplash from 'react-native-bootsplash';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {Provider, useSelector} from 'react-redux';
+import {store} from './src/store/Log/store';
 
-//import RootDirection from './src/routes/RootDirection';
+import RootDirection from './src/routes/RootDirection';
 import RootStarting from './src/routes/RootStarting';
 
-const Stack = createStackNavigator();
+const AppAuth = () => {
+  const token = useSelector(state => state.LoginReducer.authtoken);
+
+  console.log(token);
+  return (
+    <NavigationContainer>
+      {token != null ? <RootDirection /> : <RootStarting />}
+    </NavigationContainer>
+  );
+};
 
 const App = () => {
   useEffect(() => {
     setTimeout(() => {
       RNBootSplash.hide();
-    }, 800);
+    }, 100);
   }, []);
-  
 
   return (
-    <NavigationContainer>
-      <RootStarting />
-      {/*<RootDirection />*/}
-      {/*<Stack.Navigator>
-        <Stack.Screen
-          name="RDirection"
-          component={RDirection}
-          options={{header: () => null}}
-        />
-        <Stack.Screen
-          name="Direction"
-          component={Direction}
-          options={{header: () => null}}
-        />
-        <Stack.Screen
-          name="Bus"
-          component={Bus}
-          options={({navigation}) => ({
-            header: () => (
-              <TitleHeaderBar
-                Title="Bus de Noblesse voyage"
-                onPress={() => {
-                  navigation.goBack();
-                }}
-              />
-            ),
-          })}
-        />
-        <Stack.Screen
-          name="Setting"
-          component={Setting}
-          options={{
-            header: (props) => (
-              <TitleHeaderBar
-                Title="Paramètres"
-                onPress={() => {
-                  props.navigation.goBack();
-                }}
-              />
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="Chauffeur"
-          component={Chauffeur}
-          options={{
-            header: (props) => (
-              <TitleHeaderBar
-                Title="Chauffeurs de l'agence"
-                onPress={() => {
-                  props.navigation.goBack();
-                }}
-              />
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="Busconfiguration"
-          component={Busconfiguration}
-          options={{
-            header: (props) => (
-              <TitleHeaderBar
-                Title="Gestion de bus de l'agence"
-                onPress={() => {
-                  props.navigation.goBack();
-                }}
-              />
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="Ticketconfiguration"
-          component={Ticketconfiguration}
-          options={{
-            header: (props) => (
-              <TitleHeaderBar
-                Title="Type de ticket de l'agence"
-                onPress={() => {
-                  props.navigation.goBack();
-                }}
-              />
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="Courrier"
-          component={Courrier}
-          options={{
-            header: (props) => (
-              <TitleHeaderBar
-                Title="Service courrier de l'agence"
-                onPress={() => {
-                  props.navigation.goBack();
-                }}
-              />
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="Caisse"
-          component={Caisse}
-          options={{
-            header: (props) => (
-              <TitleHeaderBar
-                Title="Service caisse de l'agence"
-                onPress={() => {
-                  props.navigation.goBack();
-                }}
-              />
-            ),
-          }}
-        /> 
-        <Stack.Screen
-          name="Transport"
-          component={Transport}
-          options={{
-            header: (props) => (
-              <TitleHeaderBar
-                Title="Service Transport de l'agence"
-                onPress={() => {
-                  props.navigation.goBack();
-                }}
-              />
-            ),
-          }}
-        /> 
-        <Stack.Screen
-          name="Trajet"
-          component={Trajet}
-          options={{
-            header: (props) => (
-              <TitleHeaderBar
-                Title="Etablir les trajets de l'agence"
-                onPress={() => {
-                  props.navigation.goBack();
-                }}
-              />
-            ),
-          }}
-        /> 
-      </Stack.Navigator>*/}
-    </NavigationContainer>
+    <Provider store={store}>
+      <AppAuth />
+    </Provider>
   );
 };
 
