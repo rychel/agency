492ca72807a53b57bac1737ef70ad2da239db5a5
Maxence <maxrychel100@gmail.com>
2022-08-06 00:01:31 +0200

    Replace special regex char by letter char to v0.0.1 acxios

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -2,6 +2,6 @@ export const validEmail = new RegExp(
     '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
 );
 
-export const validSpecials = new RegExp(/[?,;.:/!§ù%*µ¨$\.\.|()=+-_}\]{[`@°&"\\^\.']/);
+export const validLettre = new RegExp(/[A-Za-z]/);
 
 export const validChiffre = new RegExp(/[0-9]/);
