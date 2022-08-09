c5106aff7251db0b6343bc602ff3f2df8c30fd21
Maxence <maxrychel100@gmail.com>
2022-08-09 05:19:18 +0200

    Update Getstarting to final v0.0.1 acios

--- src/screens/Root/Getstarting.js
+++ src/screens/Root/Getstarting.js
@@ -7,16 +7,22 @@ import {
   Animated,
   StatusBar,
   Image,
+  ScrollView,
 } from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faLeftLong} from '@fortawesome/free-solid-svg-icons';
+import {
+  faLeftLong,
+  faBezierCurve,
+  faClock,
+  faMinus,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Getstarting = ({navigation}) => {
-  const StartSpinSticker = useRef(new Animated.Value(-45)).current;
-  const FlickedTopContain = useRef(new Animated.Value(0)).current;
-  const FlickedBottomContain = useRef(new Animated.Value(0)).current;
+  const NouveauMoveMaker = useRef(new Animated.Value(-100)).current;
+  const ContsMoveMaker = useRef(new Animated.Value(30)).current;
+  const CCMoveMaker = useRef(new Animated.Value(0)).current;
 
   const gotoBackHome = () => {
     navigation.goBack('Launch Home');
@@ -32,31 +38,26 @@ const Getstarting = ({navigation}) => {
 
   useEffect(() => {
     Animated.sequence([
-      Animated.timing(StartSpinSticker, {
-        toValue: 25,
+      Animated.timing(NouveauMoveMaker, {
+        toValue: 2,
+        duration: 200,
+        useNativeDriver: true,
+      }),
+      Animated.timing(ContsMoveMaker, {
+        toValue: 2,
         duration: 300,
         useNativeDriver: true,
       }),
-      Animated.timing(FlickedTopContain, {
-        toValue: 100,
-        duration: 200,
-        useNativeDriver: true,
-      }),
-      Animated.timing(FlickedBottomContain, {
-        toValue: 30,
-        duration: 200,
+      Animated.timing(CCMoveMaker, {
+        toValue: 1,
+        duration: 500,
         useNativeDriver: true,
       }),
     ]).start();
-  }, [StartSpinSticker, FlickedTopContain, FlickedBottomContain]);
+  }, [NouveauMoveMaker, ContsMoveMaker, CCMoveMaker]);
 
   return (
-    <LinearGradient
-      start={{x: 0.0, y: 0.25}}
-      end={{x: 0.4, y: 1.0}}
-      locations={[0.79, 0.2]}
-      colors={['#7cc3bc', 'white']}
-      style={[styles.ui_splash_todo_contain]}>
+    <ScrollView style={styles.ui_splash_todo_contain}>
       <StatusBar
         animated={true}
         backgroundColor="#7cc3bc"
@@ -64,109 +65,159 @@ const Getstarting = ({navigation}) => {
         showHideTransition="fade"
         hidden={false}
       />
-      <View style={styles.ui_splash_contain_widget_header_acxios}>
-        <Animated.View
-          style={{
-            transform: [{translateY: StartSpinSticker}],
-          }}>
-          <Text style={styles.ui_splash_contain_widget_acxios}>Oulala !</Text>
-        </Animated.View>
-        <View style={styles.ui_splash_below_title_message}>
-          <Text style={styles.ui_splash_below_text_message}>
-            Maintenant gérer tout à partir de votre téléphone mobile
-          </Text>
-        </View>
-        <View style={styles.ui_splash_below_title_message2}>
-          <TouchableOpacity
-            style={styles.ui_splash_contain_go_back_button}
-            activeOpacity={0.6}
-            onPress={gotoBackHome}>
-            <FontAwesomeIcon icon={faLeftLong} size={24} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.ui_splash_button_goto_starting}
-            activeOpacity={0.8}
-            onPress={gotoGrantstarting}>
-            <Text style={styles.ui_splash_button_goto_text_starting}>
-              creer une agence
-            </Text>
-          </TouchableOpacity>
+      <View style={styles.ui_splash_contain_welcome_header_message}>
+        <View style={styles.ui_splash_contain_welcome_globe_conts}>
+          <Animated.View
+            style={[
+              styles.ui_splash_contain_globe_logo_welcome_message,
+              {transform: [{translateX: NouveauMoveMaker}]},
+            ]}>
+            <Text style={styles.ui_splash_contain_widget_acxios}>NOUVEAU</Text>
+          </Animated.View>
+          <View style={styles.ui_splash_contain_fuck_logo_welcome_message}>
+            <Image
+              source={require('../../../assets/Hi.jpg')}
+              style={styles.ui_splash_contain_logo_welcome_message}
+              resizeMode="contain"
+            />
+            <Animated.View
+              style={[
+                styles.ui_splash_contain_welcome_body_conts,
+                {transform: [{translateY: ContsMoveMaker}]},
+              ]}></Animated.View>
+          </View>
         </View>
       </View>
-      <View style={styles.ui_splash_below_title_message3}>
+      <View style={styles.ui_splash_below_title_message}>
+        <Text style={styles.ui_splash_below_text_message}>
+          Concernant l'agence, vous voulez peut être
+        </Text>
+      </View>
+      <Animated.View
+        style={[styles.ui_splash_below_title_message3, {opacity: CCMoveMaker}]}>
         <TouchableOpacity
           activeOpacity={0.6}
-          style={styles.ui_splash_box_create_title_message}>
+          style={styles.ui_splash_box_create_title_message}
+          onPress={gotoGConnecting}>
+          <FontAwesomeIcon icon={faClock} color="white" />
           <Text style={styles.ui_splash_contain_other_button_goto}>
-            creer un autre compte
+            me connecter.
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.ui_splash_contain_header_connect_button}
           activeOpacity={0.6}
-          onPress={gotoGConnecting}>
+          onPress={gotoGrantstarting}>
+          <FontAwesomeIcon icon={faBezierCurve} color="white" />
           <Text style={styles.ui_splash_contain_connect_button}>
-            Je me connecte !
+            créez une !
+          </Text>
+        </TouchableOpacity>
+      </Animated.View>
+      <View style={styles.ui_splash_below_title_message2}>
+        <TouchableOpacity
+          style={styles.ui_splash_contain_go_back_button}
+          activeOpacity={0.6}
+          onPress={gotoBackHome}>
+          <FontAwesomeIcon icon={faLeftLong} size={24} color="#000000cc" />
+        </TouchableOpacity>
+        <FontAwesomeIcon
+          icon={faMinus}
+          size={22}
+          color="#0000009c"
+          style={styles.ui_splash_support_slips_temps}
+        />
+        <TouchableOpacity
+          style={styles.ui_splash_button_goto_starting}
+          activeOpacity={0.8}>
+          <Text style={styles.ui_splash_button_goto_text_starting}>
+            creer un compte associé à une agence
           </Text>
         </TouchableOpacity>
       </View>
-    </LinearGradient>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   ui_splash_todo_contain: {
     height: Dimensions.get('window').height,
+    backgroundColor: 'white',
   },
-  ui_splash_contain_widget_header_acxios: {
+  ui_splash_contain_welcome_header_message: {
     width: '100%',
-    height: 360,
-    marginBottom: 10,
-    borderRadius: 30,
+    height: 160,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.5,
+    borderColor: '#00000017',
+  },
+  ui_splash_contain_welcome_globe_conts: {
+    width: 265,
+    height: 160,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    overflow: 'hidden',
   },
   ui_splash_contain_widget_acxios: {
-    width: 270,
+    fontSize: 30,
+    fontFamily: 'Roboto-Thin',
+  },
+  ui_splash_contain_globe_logo_welcome_message: {
+    width: 160,
     height: 90,
     alignItems: 'center',
     justifyContent: 'center',
-    alignSelf: 'center',
-    marginTop: 20,
-    marginBottom: 10,
-    fontSize: 40,
-    color: 'white',
-    fontFamily: 'Nunito-ExtraBold',
-    fontWeight: '700',
-    textShadowColor: 'black',
-    borderWidth: 0.3,
-    borderColor: 'white',
+    backgroundColor: '#ffeb3b42',
+    transform: [{translateX: -100}],
+  },
+  ui_splash_contain_fuck_logo_welcome_message: {
+    width: 105,
+    height: 160,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+  ui_splash_contain_logo_welcome_message: {
+    width: 95,
+    height: 90,
+    top: 50,
+  },
+  ui_splash_contain_welcome_body_conts: {
+    width: 90,
+    height: 90,
+    borderWidth: 1,
+    borderColor: '#1010102e',
+    borderRadius: 100,
+    top: 55,
   },
   body: {
     color: 'red',
     transform: [{rotate: '-45deg'}],
   },
   ui_splash_below_title_message: {
-    width: '90%',
-    height: 60,
-    marginTop: 40,
-    alignSelf: 'center',
+    width: '100%',
+    height: 40,
+    marginTop: 10,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 5,
   },
   ui_splash_below_title_message2: {
-    width: '90%',
-    height: 60,
-    marginTop: 40,
+    width: '100%',
+    height: 40,
+    marginTop: 20,
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'transparent',
     flexDirection: 'row',
   },
   ui_splash_below_title_message3: {
     width: '100%',
     height: 50,
-    marginTop: 124,
+    marginTop: 14,
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'space-between',
@@ -174,56 +225,52 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   ui_splash_below_text_message: {
-    fontSize: 22,
-    fontWeight: '300',
-    fontFamily: 'Nunito-Light',
-    color: 'white',
+    fontSize: 16,
+    fontFamily: 'Hind-Light',
+    color: '#000000b3',
   },
   ui_splash_box_create_title_message: {
-    width: 150,
-    height: 35,
+    width: 125,
+    height: 40,
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 100,
+    borderRadius: 10,
     left: 5,
-    borderWidth: 0.5,
-    borderColor: '#2196f3',
+    backgroundColor: '#00bcd42b',
+    flexDirection: 'row',
   },
   ui_splash_button_goto_starting: {
-    width: 130,
     height: 33,
     textAlignVertical: 'center',
     alignSelf: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
     borderRadius: 50,
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
+  },
+  ui_splash_support_slips_temps: {
+    transform: [{rotate: '-90deg'}],
   },
   ui_splash_button_goto_text_starting: {
-    fontSize: 15,
-    fontFamily: 'Nunito-ExtraLight',
+    fontSize: 13,
+    fontFamily: 'Roboto-Light',
     marginRight: 5,
     top: -1,
     color: '#000000a1',
   },
   ui_splash_contain_other_button_goto: {
-    color: '#2196f3',
-    fontFamily: 'opensanscondensedbold'
+    color: '#0000009c',
+    fontFamily: 'Roboto-Regular',
+    left: 5,
+    top: -1,
   },
   ui_splash_contain_go_back_button: {
     width: 40,
     height: 30,
-    position: 'relative',
-    left: -15,
-    textDecorationLine: 'underline',
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    borderRadius: 100,
+    left: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    fontFamily: 'SairaCondensed-Light',
   },
   ui_splash_contain_go_back_text: {
     position: 'relative',
@@ -233,12 +280,12 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   ui_splash_contain_header_connect_button: {
-    width: 130,
-    height: 35,
+    width: 120,
+    height: 40,
     position: 'relative',
     flexDirection: 'row',
     backgroundColor: '#f44336',
-    borderRadius: 100,
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
@@ -252,6 +299,8 @@ const styles = StyleSheet.create({
     fontFamily: 'PontanoSans-Regular',
     color: 'white',
     fontSize: 15,
+    left: 5,
+    top: -1,
   },
 });
 
