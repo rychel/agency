a3532fc007127f9ba3f5e420ff73f912b8174c6b
Maxence <maxrychel100@gmail.com>
2022-08-10 16:14:59 +0200

    Create InputGs grant Title to final acxios v0.0.1

---
+++ src/components/TitleInputGs.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const TitleInputGs = props => {
+  const {Title, Subtitle} = props;
+
+  return (
+    <View style={styles.ui_splash_global_control}>
+      <Text style={styles.ui_splash_text_font_config1}>{Title}</Text>
+      <Text style={styles.ui_splash_text_font_config2}>{Subtitle}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ui_splash_global_control: {
+    width: '98%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ui_splash_text_font_config1: {
+    fontSize: 18,
+    color: '#5688d3',
+    fontFamily: 'Montserrat-VariableFont_wght',
+  },
+  ui_splash_text_font_config2: {
+    fontSize: 15,
+    color: '#f3b457',
+    fontFamily: 'FredokaOne-Regular',
+  },
+});
+
+export default TitleInputGs;
