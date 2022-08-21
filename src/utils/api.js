4c5489ae5f6f4ca8529b585483e369ecc09e4b78
Maxence <maxrychel100@gmail.com>
2022-08-22 00:50:44 +0200

    Concat path to url and replace methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,15 @@
-const Api = (paths, value) => {
-    const url = 'https://591b-129-0-76-108.eu.ngrok.io';
-    
-    if(paths == 'POST'){
-        return fetch(url, {
-            method: paths,
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({title: value})
-            }).then((res) => res.json());
-    }
-    else{
-        return fetch(url).then((res) => res.json());
-    }
-}
+const Api = (methods, value, path) => {
+  const url = 'https://9082-129-0-81-247.eu.ngrok.io/' + path;
+
+  if (paths == 'POST') {
+    return fetch(url, {
+      method: methods,
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({title: value}),
+    }).then(res => res.json());
+  } else {
+    return fetch(url).then(res => res.json());
+  }
+};
 
 export default Api;
