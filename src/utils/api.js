619141e4abb9ea0b5dd90bc7ed83efc5105c77a4
Maxence <maxrychel100@gmail.com>
2022-08-22 00:58:51 +0200

    Replace path instead method in condition

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,16 @@
 const Api = (method, value, path) => {
   const url = 'https://9082-129-0-81-247.eu.ngrok.io/' + path;
 
-  if (path == 'POST') {
+  if (method === 'POST') {
+    console.log('this method');
     return fetch(url, {
       method: method,
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({title: value}),
-    }).then(res => res.json());
+    }).then(res => res.json())
+    .then(res => console.log(res));
   } else {
+    console.log('other method');
     return fetch(url).then(res => res.json());
   }
 };
