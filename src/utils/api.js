940a7b11e46c2f929aaf91b1623d36508738d62f
Maxence <maxrychel100@gmail.com>
2022-08-27 00:43:56 +0200

    Update fake api function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,16 @@
 const Api = (method, value, path) => {
-  const url = 'https://096f-129-0-81-252.eu.ngrok.io/' + path;
+  const url = 'http://192.168.43.45:5000/' + path;
 
   if (method === 'POST') {
     return fetch(url, {
       method: method,
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({title: value}),
-    }).then(res => res.json())
-    .then(res => console.log(res));
+      body: JSON.stringify(value),
+    })
+      .then(res => res.json())
+      .then(res => {
+        return res.response;
+      });
   } else {
     return fetch(url).then(res => res.json());
   }
