3a2ccbf13959032d7b6c57d1ff3c7f0cabcfa390
Maxence <maxrychel100@gmail.com>
2022-08-27 00:49:03 +0200

    Replace label syntax for all is-exists function for Grantstarting

--- src/screens/Root/Grantstarting.js
+++ src/screens/Root/Grantstarting.js
@@ -69,7 +69,7 @@ const Grantstarting = ({navigation}) => {
       })
       .test(
         'is-exists',
-        "Votre code de sécurité n'est pas sure, essayez un autre",
+        "Votre code de sécurité n'est pas sûr, essayez un autre",
         async value => {
           try{
             const request = await fetch(
@@ -105,7 +105,7 @@ const Grantstarting = ({navigation}) => {
       })
       .test(
         'is-exists',
-        "Votre mot de passe n'est pas sure, essayez un autre",
+        "Votre mot de passe n'est pas sûr, essayez un autre",
         async value => {
           try{
             const request = await fetch(
