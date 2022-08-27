8d5d9f504ef2a7e9cf5b9f110061f63591e43d7d
Maxence <maxrychel100@gmail.com>
2022-08-27 12:03:06 +0200

    Replace global AGENCY_DIRECTION of config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,14 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export const URL = 'http://192.168.43.45:5000';
 
 /* DIRECTION TRUCKS ------------------------------ */
-export const AGENCY_DIRECTION = async (id_direction) => {
-  await fetch('http://192.168.43.45:5000/api/id_direction', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({title: id_direction}),
-  });
+
+export const AGENCY_DIRECTION = () => {
+  try {
+    AsyncStorage.getItem('token').then(async value => {
+      if (value != null) {
+        const request = await fetch(
+          'http://192.168.43.45:5000/api/get_agency_direction',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({title: parseInt(value)}),
+          },
+        );
+        const success = await request.json();
+        return success;
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+module.exports = {
+  AGENCY_DIRECTION: AGENCY_DIRECTION,
 };
 
 /* OTHER TRUCKS ------------------------------ */
