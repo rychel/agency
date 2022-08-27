2f7da4b1049a8654d3681986559d9ba313db9608
Maxence <maxrychel100@gmail.com>
2022-08-27 09:30:01 +0200

    Add global AGENCY_DIRECTION function for get agency informations of acxios

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,14 @@
 export const URL = 'http://192.168.43.45:5000';
+
+/* DIRECTION TRUCKS ------------------------------ */
+export const AGENCY_DIRECTION = async (id_direction) => {
+  await fetch('http://192.168.43.45:5000/api/id_direction', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({title: id_direction}),
+  });
+};
+
+/* OTHER TRUCKS ------------------------------ */
