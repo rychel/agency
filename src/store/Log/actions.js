4294de47982abaa5d28d33fcb542a373fe3ad774
Maxence <maxrychel100@gmail.com>
2022-08-27 08:21:27 +0200

    Add role to dispatch of appropiated actions

--- src/store/Log/actions.js
+++ src/store/Log/actions.js
@@ -1,34 +1,30 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API = 'http://192.168.8.100:5000/api/create_agency/access_token';
-
 export const Start = () => {
   return async dispatch => {
     const token = await AsyncStorage.getItem('token');
-    
-
-    if (token !== null) {
-      console.log('ok good token');
-      dispatch({type: 'LOGIN', payload: token});
-    } else {
+    const role = await AsyncStorage.getItem('role');
+    if (token !== undefined) {
+      Login(token, role);
+    }
+    else{
       console.log('not store token');
     }
   };
 };
 
-export const Login = (Username, Password) => {
+export const Login = (id, role) => {
   return async dispatch => {
-    const token = Username + Password;
-    await AsyncStorage.setItem('token', token);
-    console.log('token stored');
-    dispatch({type: 'LOGIN', payload: token});
+    await AsyncStorage.setItem('token', id);
+    await AsyncStorage.setItem('role', role);
+    dispatch({type: 'LOGIN', token: id, role: role});
   };
 };
 
 export const LogOut = () => {
   return async dispatch => {
     await AsyncStorage.removeItem('token');
-    console.log('token restored');
+    await AsyncStorage.removeItem('role');
     dispatch({type: 'LOGOUT'});
   };
 };
