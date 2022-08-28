0004f4fccded2289a3178ef657a8c8d2d732fc05
Maxence <maxrychel100@gmail.com>
2022-08-28 06:02:22 +0200

    Add Awesome loader and customize message police

--- src/components/LoadingWelcome.js
+++ src/components/LoadingWelcome.js
@@ -8,7 +8,7 @@ const LoadingWelcome = props => {
   return (
     <View style={styles.ui_splash_global_display_screen}>
       <AnimatedLottieView
-        source={require("../../assets/98556-loader.json")}
+        source={require('../../assets/98556-loader.json')}
         style={styles.ui_splash_global_activity_icon}
         autoPlay
       />
@@ -24,22 +24,27 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: 'white',
-    position: 'absolute',
-    zIndex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
   ui_splash_global_display_unhandled_text1: {
     position: 'relative',
-    fontFamily: 'Quicksand-VariableFont_wght',
-    fontSize: 14,
-    width: "65%",
+    fontFamily: 'Hind-Light',
+    fontSize: 15,
+    width: 120,
+    height: 50,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    top: -36,
+    color: '#5f6868',
   },
   ui_splash_global_activity_icon: {
-    left: -10,
-    width: 100,
-    height: 90,
+    width: 96,
+    height: 88,
+    fontSize: 124,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
