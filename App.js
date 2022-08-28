321b5944c3940704ffa3ac128c3ff69f04631f6a
Maxence <maxrychel100@gmail.com>
2022-08-28 06:05:33 +0200

    Replace waiting message props of loader

--- App.js
+++ App.js
@@ -5,26 +5,37 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Provider, useSelector, useDispatch} from 'react-redux';
 import {store} from './src/store/Log/store';
 import {Start} from './src/store/Log/actions';
-
+import LoadingWelcome from './src/components/LoadingWelcome';
 import RootDirection from './src/routes/RootDirection';
 import RootStarting from './src/routes/RootStarting';
 
 const AppAuth = () => {
+  const [loading, setLoading] = useState(true);
+
   const token = useSelector(state => state.LoginReducer.token);
   const role = useSelector(state => state.LoginReducer.role);
-  const dispatch = useDispatch();
 
-  const start = () => {
-    dispatch(Start());
-  }
-  
+  const dispatch = useDispatch();
+  const start = async () => {
+    await dispatch(Start());
+    setLoading(false);
+  };
+
   useEffect(() => {
     start();
   });
 
+  if (loading) {
+    return <LoadingWelcome Messagewaiting="patientez juste." />;
+  }
+
   return (
     <NavigationContainer>
-      {token === null ? <RootStarting /> : role === "direction" ?  <RootDirection /> : null}
+      {token === null ? (
+        <RootStarting />
+      ) : role === 'direction' ? (
+        <RootDirection />
+      ) : null}
     </NavigationContainer>
   );
 };
