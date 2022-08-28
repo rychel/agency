e48f0879a934a77b35ebfb5ecbbb1934af4b5cdf
Maxence <maxrychel100@gmail.com>
2022-08-28 04:50:56 +0200

    add Reducers's  Direction path  v0.0.1 acxios

--- src/store/Log/store.js
+++ src/store/Log/store.js
@@ -1,8 +1,10 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import LoginReducer from './reducers';
+import DirReducers from './Dir/DirReducers';
 const RootReducers = combineReducers({
   LoginReducer,
+  DirReducers,
 });
 
 export const store = createStore(RootReducers, applyMiddleware(thunk));
