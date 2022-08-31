b5106e7051c64d95d4836a597df48882511404f3
Maxence <maxrychel100@gmail.com>
2022-08-31 11:18:05 +0200

    Update item disposition

--- src/components/DrawerDirection.js
+++ src/components/DrawerDirection.js
@@ -55,11 +55,11 @@ const DrawerDirection = props => {
                 <FontAwesomeIcon
                   icon={faSignOutAlt}
                   size={17}
-                  color="#03a9f4"
+                  color="#4c444796"
                   style={styles.ui_splash_contain_log_sign_configs1}
                 />
                 <Text style={styles.ui_splash_contain_text_configs1}>
-                  Se déconnecter
+                  Fermer la session
                 </Text>
               </TouchableOpacity>
             </View>
@@ -116,19 +116,22 @@ const styles = StyleSheet.create({
   ui_splash_contain_header_deconnexion_contains: {
     width: '100%',
     height: 40,
-    backgroundColor: '#7cc3bc14',
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
+    shadowOffset: {width: -2, height: 12},
+    shadowOpacity: 2,
+    elevation: 4,
+    shadowRadius: 3,
   },
   ui_splash_contain_log_sign_configs1: {
     left: 5,
   },
   ui_splash_contain_text_configs1: {
     left: 10,
-    fontFamily: 'OpenSansCondensed-Light',
-    color: '#03a9f4',
-    fontSize: 18,
+    fontFamily: 'Hind-Light',
+    color: '#4c444796',
+    fontSize: 16.2,
   },
 });
 
