0465d8c98d4b548294b14ca21448136512a1fba4
Maxence <maxrychel100@gmail.com>
2022-09-06 18:21:38 +0200

    Add Message props of LoaderPending v0.0.1 acxios

--- src/components/LoaderPending.js
+++ src/components/LoaderPending.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {View, StyleSheet, ActivityIndicator} from 'react-native';
+import {View, StyleSheet, ActivityIndicator, Text} from 'react-native';
 
-const LoaderPending = () => {
+const LoaderPending = props => {
+  const {Message} = props;
+  
   return (
     <View style={styles.ui_splash_global_display_screen}>
-      <ActivityIndicator animating={true} color="#57f1d8" size={40} />
+      <ActivityIndicator
+        animating={true}
+        color="#57f1d8"
+        size={40}
+        style={{left: -10}}
+      />
+      <Text style={styles.ui_splash_global_display_unhandled_text1}>
+        {Message}
+      </Text>
     </View>
   );
 };
@@ -19,6 +29,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
+  },
+  ui_splash_global_display_unhandled_text1: {
+    position: 'relative',
+    fontFamily: 'Hind-Light',
+    fontSize: 20,
+    height: 50,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    color: '#57f1d8',
   },
 });
 
