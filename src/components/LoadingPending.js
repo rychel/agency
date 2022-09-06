1a57d5cec440d6f1c6beacece693038be2bf2ca8
Maxence <maxrychel100@gmail.com>
2022-09-06 03:27:11 +0200

    Add a loader for suspending screen state v0.0.1 acxios

---
+++ src/components/LoadingPending.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
+
+const LoadingPending = () => {
+  return (
+    <View style={styles.ui_splash_global_display_screen}>
+      <ActivityIndicator animating={true} color="#57f1d8" size={40} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ui_splash_global_display_screen: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#00000052',
+    position: 'absolute',
+    zIndex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+});
+
+export default LoadingPending;
