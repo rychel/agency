e47a21c85715bac52ab7706bc6b8e02153d82e1a
Maxence <maxrychel100@gmail.com>
2022-09-10 02:26:45 +0200

    Add get config transport action for config transport state of Direction part

--- src/store/Log/Dir/DirActions.js
+++ src/store/Log/Dir/DirActions.js
@@ -15,8 +15,28 @@ export const get_agency_direction = (id_direction) => {
       if(success){
         dispatch({type: 'GET_AGENCY_DIRECTION', info_agency: success});
       }
-      else{
-        console.log("not work");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
+
+export const get_config_transport = (id_direction) => {
+  return async dispatch => {
+    try {
+      const request = await fetch(
+        'http://192.168.43.45:5000/api/get_agency_direction',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({title: parseInt(id_direction)}),
+        },
+      );
+      const success = await request.json();
+      if(success){
+        dispatch({type: 'GET_CONFIG_TRANSPORT', config_transport: success});
       }
     } catch (e) {
       console.log(e);
