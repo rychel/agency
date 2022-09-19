2328e8e0fe748ad382e54417078008054b4d8c22
Maxence <maxrychel100@gmail.com>
2022-09-19 13:57:20 +0200

    Custom ConnexionStatus component and add  Isconnected and Isreached props

--- src/components/ConnexionStatus.js
+++ src/components/ConnexionStatus.js
@@ -1,16 +1,24 @@
 import React, {useEffect, useRef} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, Animated} from 'react-native';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faBars, faSearch, faWifi} from '@fortawesome/free-solid-svg-icons';
+import AnimatedLottieView from 'lottie-react-native';
 
 const ConnexionStatus = props => {
-  
+  const {Isreached, Isconnected} = props;
+
   return (
     <View style={styles.ui_splash_contain_header_globe}>
-      <View style={styles.ui_splash_contain_destination_items_globe_text2}>
-        <Text>Vous n'etes pas connecté ! </Text>
-        <FontAwesomeIcon icon={faWifi} size={20} color="white" />
-      </View>
+      {Isconnected ? null : (
+        <View style={styles.ui_splash_contain_destination_globe}>
+          <AnimatedLottieView //../../assets/97651-loading.json
+            source={require('../../assets/99109-loading.json')}
+            style={styles.ui_splash_global_activity_icon}
+            autoPlay
+          />
+          <Text style={styles.ui_splash_contain_destination_label}>
+            Tentative de connexion
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -19,10 +27,26 @@ const styles = StyleSheet.create({
   ui_splash_contain_header_globe: {
     backgroundColor: '#00CDAC',
     flexDirection: 'row',
-    height: 55,
     justifyContent: 'center',
+    alignItems: 'center',
   },
-
+  ui_splash_contain_destination_globe: {
+    height: 28,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 100,
+    justifyContent: 'space-around',
+  },
+  ui_splash_contain_destination_label: {
+    fontFamily: 'Quicksand-VariableFont_wght',
+    left: -4,
+    marginRight: 5,
+    marginLeft: 7,
+  },
+  ui_splash_global_activity_icon: {
+    width: 33,
+  },
 });
 
 export default ConnexionStatus;
