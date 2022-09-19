e591ebeb26ad6a1701ec1aca7af69db69e3fa140
Maxence <maxrychel100@gmail.com>
2022-09-19 17:58:59 +0200

    Update Caisse to Pratice

--- src/screens/Direction/Caisse.js
+++ src/screens/Direction/Caisse.js
@@ -1,36 +1,70 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
-
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {get_config_transport} from '../../store/Log/Dir/DirActions';
 import ButtonAddingItems from '../../components/ButtonAddingItems';
 import NoItemStatus from '../../components/NoItemStatus';
 import ChooseCaissier from '../../components/ChooseCaissier';
+import ErrorItemStatus from '../../components/ErrorItemStatus';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faPlus, faUserSlash} from '@fortawesome/free-solid-svg-icons';
 
-const Caisse = (props) => {
-  
+const Caisse = props => {
+  //<ChooseCaissier />
+  const getConfig_transport = () => {
+    try {
+      AsyncStorage.getItem('token').then(value => {
+        dispatch(get_config_transport(value));
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const config_transport = useSelector(
+    state => state.DirReducers.config_transport,
+  );
+  const dispatch = useDispatch();
+  const [icoindicate, setIcoindicate] = useState(-30);
+
+  useEffect(() => {
+    getConfig_transport();
+    console.log(config_transport);
+  });
+
+  const addMaid = () => {
+    setIcoindicate(10);
+    setTimeout(() => {
+      setIcoindicate(-30);
+    }, 2000);
+  };
+
   return (
     <View style={styles.ui_splash_contain_header_globe}>
       <NoItemStatus
-        Titleico="user-slash"
-        Titlestatus="aucune caissière enregistrée"
+        Titleico={faUserSlash}
+        Titlestatus="aucun(e) caissi(er)(ère) enregistré(e)"
       />
-      <ButtonAddingItems titleico='plus' titlebutton='ajouter' borderrounds={50} />
-      <ChooseCaissier />
+      <ErrorItemStatus
+        Titlestatus="Avant de continuer, choisir le type de ticket qu'utilisera votre agence. Vous pouvez le faire dans l'onglet Ticket."
+        TruthFor={config_transport[0]?.TypeTicket}
+      />
+      <ButtonAddingItems
+        titleico={faPlus}
+        titlebutton="ajouter"
+        borderrounds={50}
+        onOpen={addMaid}
+      />
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   ui_splash_contain_header_globe: {
     width: '100%',
     height: Dimensions.get('window').height,
-    backgroundColor: '#f1bfbf14',
-  }
+    backgroundColor: 'white',
+  },
 });
 
 export default Caisse;
