d3873bcb0d8f92b59d078c9ac9da3912936e98ee
Maxence <maxrychel100@gmail.com>
2022-09-22 00:37:10 +0200

    Remove imposible dispatch action

--- src/store/Log/Dir/DirActions.js
+++ src/store/Log/Dir/DirActions.js
@@ -45,7 +45,7 @@ export const get_config_transport = (id_direction) => {
 export const update_config_transport = (id_direction, data_updated) => {
   return async dispatch => {
     try {
-      const request = await fetch(
+      fetch(
         URL + '/api/update_config_transport',
         {
           method: 'POST',
@@ -55,8 +55,6 @@ export const update_config_transport = (id_direction, data_updated) => {
           body: JSON.stringify({title: parseInt(id_direction), value: data_updated}),
         },
       );
-      const success = await request.json();
-      dispatch({type: 'GET_CONFIG_TRANSPORT', config_transport: success});
     } catch (e) {
       console.log(e);
     }
