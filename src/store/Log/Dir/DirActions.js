801bbfcb2f4f9336fa32d7221174f954f1245b13
Maxence <maxrychel100@gmail.com>
2022-09-21 21:55:09 +0200

    Add update config transport function

--- src/store/Log/Dir/DirActions.js
+++ src/store/Log/Dir/DirActions.js
@@ -45,3 +45,26 @@ export const get_config_transport = (id_direction) => {
     }
   };
 };
+
+export const update_config_transport = (id_direction, data_updated) => {
+  return async dispatch => {
+    try {
+      const request = await fetch(
+        URL + '/api/update_config_transport',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({title: parseInt(id_direction), value: data_updated}),
+        },
+      );
+      const success = await request.json();
+      if(success){
+        dispatch({type: 'GET_CONFIG_TRANSPORT', config_transport: success});
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
