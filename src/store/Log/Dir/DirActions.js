5db251803abc97f5951ebbd9e98889804a834cdd
Maxence <maxrychel100@gmail.com>
2022-09-22 18:43:13 +0200

    Add target point action function for target_point state of Reducers

--- src/store/Log/Dir/DirActions.js
+++ src/store/Log/Dir/DirActions.js
@@ -1,18 +1,15 @@
 export const URL = 'http://192.168.8.100:5000';
 
-export const get_agency_direction = (id_direction) => {
+export const get_agency_direction = id_direction => {
   return async dispatch => {
     try {
-      const request = await fetch(
-        URL + '/api/get_agency_direction',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({title: parseInt(id_direction)}),
+      const request = await fetch(URL + '/api/get_agency_direction', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      );
+        body: JSON.stringify({title: parseInt(id_direction)}),
+      });
       const success = await request.json();
       dispatch({type: 'GET_AGENCY_DIRECTION', info_agency: success});
     } catch (e) {
@@ -21,19 +18,16 @@ export const get_agency_direction = (id_direction) => {
   };
 };
 
-export const get_config_transport = (id_direction) => {
+export const get_config_transport = id_direction => {
   return async dispatch => {
     try {
-      const request = await fetch(
-        URL + '/api/get_config_transport',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({title: parseInt(id_direction)}),
+      const request = await fetch(URL + '/api/get_config_transport', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      );
+        body: JSON.stringify({title: parseInt(id_direction)}),
+      });
       const success = await request.json();
       dispatch({type: 'GET_CONFIG_TRANSPORT', config_transport: success});
     } catch (e) {
@@ -45,16 +39,16 @@ export const get_config_transport = (id_direction) => {
 export const update_config_transport = (id_direction, data_updated) => {
   return async dispatch => {
     try {
-      fetch(
-        URL + '/api/update_config_transport',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({title: parseInt(id_direction), value: data_updated}),
+      fetch(URL + '/api/update_config_transport', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      );
+        body: JSON.stringify({
+          title: parseInt(id_direction),
+          value: data_updated,
+        }),
+      });
     } catch (e) {
       console.log(e);
     }
@@ -64,16 +58,16 @@ export const update_config_transport = (id_direction, data_updated) => {
 export const update_site_localisation = (id_direction, data_updated) => {
   return async dispatch => {
     try {
-      fetch(
-        URL + '/api/update_site_localisation',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({title: parseInt(id_direction), value: data_updated}),
+      fetch(URL + '/api/update_site_localisation', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      );
+        body: JSON.stringify({
+          title: parseInt(id_direction),
+          value: data_updated,
+        }),
+      });
     } catch (e) {
       console.log(e);
     }
@@ -83,16 +77,31 @@ export const update_site_localisation = (id_direction, data_updated) => {
 export const add_target_point = (id_direction, data) => {
   return async dispatch => {
     try {
-      fetch(
-        URL + '/api/add_target_point',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({title: parseInt(id_direction), value: data}),
+      fetch(URL + '/api/add_target_point', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      );
+        body: JSON.stringify({title: parseInt(id_direction), value: data}),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
+
+export const get_target_point = id_direction => {
+  return async dispatch => {
+    try {
+      const request = await fetch(URL + '/api/get_target_point', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({title: parseInt(id_direction)}),
+      });
+      const success = await request.json();
+      dispatch({type: 'GET_TARGET_POINT', target_point: success});
     } catch (e) {
       console.log(e);
     }
