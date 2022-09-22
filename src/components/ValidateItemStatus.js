da553da666da9f8672196f26d6610adc9cc1de6b
Maxence <maxrychel100@gmail.com>
2022-09-22 20:06:58 +0200

    Change displaying theme of render

--- src/components/ValidateItemStatus.js
+++ src/components/ValidateItemStatus.js
@@ -8,7 +8,7 @@ const ValidateItemStatus = props => {
   return (
     <View style={styles.ui_splash_contain_header_administration_item_config}>
       <View style={styles.ui_splash_contain_header_administration_awesome_baps}>
-        <FontAwesomeIcon icon={Titleico} size={20} color="#d1d1d1" />
+        <FontAwesomeIcon icon={Titleico} size={25} color="white" />
       </View>
       <Text
         style={styles.ui_splash_contain_header_administration_awesome_config}>
@@ -26,18 +26,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     margin: 1,
     flexDirection: 'row',
-    paddingRight: 20,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: 0,
+    alignItems: 'center',
   },
   ui_splash_contain_header_administration_awesome_config: {
     margin: 0,
-    fontSize: 18,
+    fontSize: 20,
     height: 40,
     color: '#706e6e',
-    fontFamily: 'Rajdhani-Light',
+    fontFamily: 'Roboto-Thin',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 22,
+    marginTop: 15,
   },
   ui_splash_contain_header_administration_awesome_caps: {
     width: 25,
@@ -53,8 +56,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     width: '13.33%',
-    marginTop: 14,
+    marginTop: 2,
     marginLeft: 5,
+    borderRadius: 100,
+    backgroundColor: '#8bc34a',
+    height: 40,
+    marginRight: 10,
   },
   ui_splash_global_bus_soup_contain_registered_global_contain_title: {
     width: '100%',
