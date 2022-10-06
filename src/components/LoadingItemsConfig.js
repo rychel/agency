7b40eaf818398d4082b51e0bf1401533d0a3f23e
Maxence <maxrychel100@gmail.com>
2022-10-06 22:40:12 +0200

    Add ActivityIndicator instead of loading every items and Rename component

---
+++ src/components/LoadingItemsConfig.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
+
+const LoadingItemsConfig = props => {
+  return (
+    <View style={styles.ui_splash_contain_header_administration_item_config}>
+      <View style={styles.ui_splash_contain_header_administration_awesome_config}>
+        <ActivityIndicator color="#ffb03b" size={35}  />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ui_splash_contain_header_administration_item_config: {
+    width: '99%',
+    height: 60,
+    flexDirection: 'row',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 150,
+    zIndex: 2,
+  },
+  ui_splash_contain_header_administration_awesome_config: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#c1c1c11f',
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default LoadingItemsConfig;
