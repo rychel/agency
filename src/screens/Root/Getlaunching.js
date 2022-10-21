47e483ba86aebd5a56a4fc7f737dad1e4b6a0843
Maxence <maxrychel100@gmail.com>
2022-10-21 04:29:44 +0200

    ADD NEW FEATURES v1 HELL'O TRAVEL GETLAUNCHING SCREEN

--- src/screens/Root/Getlaunching.js
+++ src/screens/Root/Getlaunching.js
@@ -5,112 +5,130 @@ import {
   StyleSheet,
   Dimensions,
   Animated,
-  ScrollView,
   Image,
 } from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
+import Svg, {Path} from 'react-native-svg';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faAngleRight} from '@fortawesome/free-solid-svg-icons';
+import { ease } from 'react-native/Libraries/Animated/Easing';
 
 const Getlaunching = ({navigation}) => {
-  const StartWelcomeBrave = useRef(new Animated.Value(0)).current;
-  const EndWelcomeBrave = useRef(new Animated.Value(200)).current;
-  const FlickedLogo = useRef(new Animated.Value(0)).current;
+  const StatWithSvg = useRef(new Animated.Value(-400)).current;
+  const PinkDetails = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    Animated.sequence([
-      Animated.timing(StartWelcomeBrave, {
-        toValue: -150,
-        duration: 300,
-        useNativeDriver: true,
-      }),
-      Animated.timing(FlickedLogo, {
-        toValue: 50,
-        duration: 400,
-        useNativeDriver: true,
-      }),
-      Animated.timing(EndWelcomeBrave, {
-        toValue: 0,
-        duration: 100,
-        useNativeDriver: true,
-      }),
-    ]).start();
-  }, [EndWelcomeBrave, FlickedLogo, StartWelcomeBrave]);
+    Animated.timing(StatWithSvg, {
+      toValue: 0,
+      duration: 800,
+      useNativeDriver: true,
+    }).start();
+    Animated.timing(PinkDetails, {
+      toValue: 1,
+      duration: 1200,
+      useNativeDriver: true,
+      easing: ease,
+    }).start();
+  }, [StatWithSvg, PinkDetails]);
 
   const goWelcomeBrave = () => {
     navigation.navigate('Get starting');
   };
 
   return (
-    <ScrollView>
-      <LinearGradient
-        style={styles.ui_splash_global_app_header_contain}
-        start={{x: 0.0, y: 0.25}}
-        end={{x: 0.5, y: 1.0}}
-        locations={[0.05, 0.6]}
-        colors={['#00bcd4', '#00bcd4de']}>
-        <View style={styles.ui_splash_contain_header_logo_started}>
-          <Animated.Image
-            source={require('../../../assets/Logo1.png')}
-            resizeMode="contain"
-            style={[
-              styles.ui_splash_logo_started,
-              {borderBottomLeftRadius: FlickedLogo},
-            ]}
-          />
-        </View>
+    <View style={styles.ui_splash_global_app_contain}>
+      <View style={styles.ui_splash_global_app_header_contain}>
+        <View style={styles.ui_splash_ico_distributed}></View>
+      </View>
+      <View style={styles.ui_splash_global_app_footer_contain}>
         <Animated.View
           style={[
-            styles.ui_splash_started_title_welcome,
-            {transform: [{translateY: StartWelcomeBrave}]},
+            styles.ui_splash_contain_viewer_svg_display,
+            {transform: [{translateX: StatWithSvg}]},
           ]}>
-          <Text style={styles.ui_splash_started_font_config1}>
-            Vous souhaite la bienvenue au sein de son service d'hébergement
-            d'agence.
-          </Text>
+          <Svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            style={styles.ui_splash_svg_display_started}>
+            <Path
+              fill="white"
+              stroke="#c1c1c1ad"
+              strokeWidth="4"
+              d="M762.2,189.9c-17.4-27.1-38.1-52.2-61.3-74.5c-23.9-22.9-50.8-43.1-79.8-59.8C528.3,2,419.9-13.5,315.9,11.9 c-50.6,12.4-97.8,33.8-140.2,63.6C131.9,106.3,95,144.9,65.9,190.1c-0.1,0.2-0.2,0.3-0.3,0.5c-3.4,5.8-1.4,13,4.4,16.4l308.3,178 c8.4-10.4,21.3-17.1,35.8-17.1c14.4,0,27.2,6.6,35.6,16.9l308.4-178c1.9-1.1,3.4-2.6,4.5-4.4C764.8,198.5,764.7,193.7,762.2,189.9z"
+            />
+          </Svg>
         </Animated.View>
-      </LinearGradient>
+      </View>
       <Animated.View
         style={[
           styles.ui_splash_started_title_contracts,
-          {transform: [{translateY: EndWelcomeBrave}]},
+          {opacity: PinkDetails},
         ]}>
-        <Text style={styles.ui_splash_started_text_contracts1}>
-          Resté connecté à votre agence !
-        </Text>
-        <Text style={styles.ui_splash_started_contain_text_contracts2}>
-          Garder les clés en main.
-        </Text>
-        <View style={styles.ui_splash_started_container_button_welcome}>
-          <TouchableOpacity
-            activeOpacity={0.7}
-            onPress={goWelcomeBrave}
+        <View style={styles.ui_splash_started_name_app_viewer}>
+          <Text style={styles.ui_splash_started_name_app_part1}>Hell</Text>
+          <Text style={styles.ui_splash_started_name_app_part2}>'</Text>
+          <Text style={styles.ui_splash_started_name_app_part1}>o Travel</Text>
+        </View>
+        <View style={styles.ui_splash_started_name_app_descrip_viewer}>
+          <Text style={styles.ui_splash_started_contain_text_contracts2}>
+            L'application de gestion
+            <Text style={styles.ui_splash_started_text_contracts1}>
+              {' '}
+              d'agence de transport
+            </Text>{' '}
+            par bus
+            <Text style={styles.ui_splash_started_text_contracts1}>
+              {' '}
+              en temps réel.
+            </Text>
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={goWelcomeBrave}
+          style={[
+            styles.ui_splash_started_button_welcome,
+            styles.ui_splash_started_contain_button_welcome,
+          ]}>
+          <LinearGradient
+            start={{x: 0.0, y: 0.25}}
+            end={{x: 0.5, y: 1.0}}
+            locations={[0.05, 0.6]}
+            colors={['#707be5', '#5e6be3']}
             style={styles.ui_splash_started_button_welcome}>
             <Text style={styles.ui_splash_started_button_text}>
-              débutez maintenant !
+              découvrez maintenant
             </Text>
-            <Text style={styles.ui_splash_started_button_text2}>
-              débutez maintenant !
-            </Text>
-          </TouchableOpacity>
-        </View>
+            <FontAwesomeIcon icon={faAngleRight} color="white" size={20} />
+          </LinearGradient>
+        </TouchableOpacity>
       </Animated.View>
-    </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   ui_splash_global_app_contain: {
+    width: '100%',
     height: Dimensions.get('window').height,
+    backgroundColor: '#ff9800',
   },
   ui_splash_global_app_header_contain: {
-    height: 430,
+    height: 190,
     width: '100%',
-    marginBottom: 20,
-    alignContent: 'center',
-    justifyContent: 'space-between',
-    position: 'relative',
-    alignSelf: 'center',
-    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ui_splash_ico_distributed: {
+    width: 95,
+    height: 95,
+    borderRadius: 100,
+    backgroundColor: 'white',
+  },
+  ui_splash_global_app_footer_contain: {
+    height: 355,
+    width: '100%',
   },
   ui_splash_contain_header_logo_started: {
     alignSelf: 'center',
@@ -123,129 +141,93 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
     top: 20,
   },
-  ui_splash_logo_started: {
-    alignSelf: 'center',
-    width: 95,
-    height: 95,
-    borderWidth: 1,
-    borderColor: 'white',
-    borderRadius: 100,
-    backgroundColor: 'white',
-  },
-  ui_splash_started_title_welcome: {
+  ui_splash_contain_viewer_svg_display: {
+    width: '100%',
+    height: 355,
+    overflow: 'hidden',
     alignItems: 'center',
     justifyContent: 'center',
-    width: '85%',
-    alignSelf: 'center',
-    height: 50,
-    backgroundColor: '#00bcd4de',
-    marginTop: 10,
-    marginBottom: 10,
-    position: 'relative',
-    fontFamily: 'cursive',
-    fontSize: 20,
-    borderRadius: 100,
-    top: 100,
-    padding: 5,
-    shadowOffset: {width: 35, height: -31},
-    elevation: 0.3,
+  },
+  ui_splash_svg_display_started: {
+    width: 1100,
+    height: 800,
+    transform: [{rotate: '50deg'}],
+    left: 117,
+    top: 270,
+    zIndex: 2,
   },
   ui_splash_started_font_config1: {
     fontSize: 14,
     color: 'white',
   },
   ui_splash_started_title_contracts: {
-    width: '100%',
-    height: 153,
-    marginTop: -58,
+    width: '80%',
+    height: 180,
+    marginTop: 8,
     position: 'relative',
-    backgroundColor: 'white',
-    borderTopStartRadius: 40,
-    borderTopEndRadius: 40,
+    borderTopRightRadius: 100,
+    borderBottomRightRadius: 100,
+    top: -190,
+  },
+  ui_splash_started_name_app_viewer: {
+    flexDirection: 'row',
+    height: 50,
+    top: -30,
+  },
+  ui_splash_started_name_app_part1: {
+    fontSize: 26,
+    color: '#817d7d',
+    left: 10,
+    fontFamily: 'Quicksand-VariableFont_wght',
+    fontWeight: '700',
+    top: 8,
+  },
+  ui_splash_started_name_app_part2: {
+    fontSize: 45,
+    color: '#f44336',
+    left: 10,
+    fontFamily: 'Quicksand-VariableFont_wght',
+    fontWeight: '700',
+    transform: [{rotate: '10deg'}],
+  },
+  ui_splash_started_name_app_descrip_viewer: {
+    flexDirection: 'row',
+    height: 60,
+    width: '100%',
+    alignItems: 'center',
   },
   ui_splash_started_text_contracts1: {
     color: '#000000c4',
     fontSize: 16,
     fontFamily: 'FontAwesome',
-    textAlign: 'left',
-    position: 'relative',
-    top: 5,
-    marginTop: 18,
-    left: 12,
-    height: 20,
   },
   ui_splash_started_contain_text_contracts2: {
-    left: 10,
-    fontSize: 14,
-    width: 160,
-    padding: 3,
-    borderRadius: 100,
-    fontFamily: 'Rajdhani-Light',
-  },
-  ui_splash_started_text_contracts2: {
-    color: '#f44336bd',
-    textDecorationLine: 'underline',
     fontSize: 15,
-    width: '88%',
-    fontWeight: '300',
-    fontFamily: 'SairaCondensed-Medium',
-    marginTop: 5,
+    fontFamily: 'NotoSans-Regular',
+    left: 10,
+    padding: 2,
+    color: '#02e5be',
   },
-  ui_splash_started_container_button_welcome: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 100,
-    width: 200,
-    position: 'relative',
-    left: -8,
-    fontFamily: 'cursive',
-    fontSize: 20,
-    borderRadius: 5,
-    top: -5,
-    alignSelf: 'flex-end',
+  ui_splash_started_contain_button_welcome: {
+    left: 10,
+    top: 12,
   },
   ui_splash_started_button_welcome: {
-    width: 175,
-    height: 43,
+    width: 185,
+    height: 45,
     alignItems: 'center',
-    flexDirection: 'row',
     justifyContent: 'center',
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
-    borderTopRightRadius: 30,
-    borderTopLeftRadius: 30,
-    left: -10,
-    top: 10,
-    alignSelf: 'flex-end',
-    flexDirection: 'column',
+    borderRadius: 6,
+    flexDirection: 'row',
   },
   ui_splash_started_button_text: {
     fontSize: 15,
     marginRight: 5,
     fontFamily: 'Quicksand-VariableFont_wght',
     position: 'relative',
-    color: '#000000a6',
-    backgroundColor: '#02e5be',
-    padding: 6,
-    borderRadius: 100,
-    zIndex: 1,
-    top: 8,
-  },
-  ui_splash_started_button_text2: {
-    fontSize: 15,
-    marginRight: 5,
-    fontFamily: 'Quicksand-VariableFont_wght',
-    position: 'relative',
-    color: '#f4433600',
-    backgroundColor: '#f443365c',
-    borderRadius: 100,
-    top: -11,
-    zIndex: 0,
-    transform: [{rotate: '-2deg'}],
-  },
-  ui_splash_started_logo_welcome_speak: {
-    width: 90,
-    height: 90,
+    color: 'white',
+    top: -1,
+    left: 3,
   },
 });
 
